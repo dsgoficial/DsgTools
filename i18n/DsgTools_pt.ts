27dce4b75b641ebffc95580469cba0eaea7f7885
lcoandrade <lcoandrade@gmail.com>
2014-11-06 18:31:02 -0200

    First translations ready (i18n)

---
+++ i18n/DsgTools_pt.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_BR" sourcelanguage="en_US">
+<context>
+    <name>DsgTools</name>
+    <message>
+        <location filename="dsg_tools.py" line="304"/>
+        <source>&amp;DSG Tools</source>
+        <translation>Ferramentas da DSG</translation>
+    </message>
+    <message>
+        <location filename="dsg_tools.py" line="81"/>
+        <source>DsgTools</source>
+        <translation>DsgTools</translation>
+    </message>
+    <message>
+        <location filename="dsg_tools.py" line="197"/>
+        <source>DSG Tools</source>
+        <translation>Ferramentas da DSG</translation>
+    </message>
+    <message>
+        <location filename="dsg_tools.py" line="202"/>
+        <source>Database Tools</source>
+        <translation>Ferramentas de Banco</translation>
+    </message>
+    <message>
+        <location filename="dsg_tools.py" line="203"/>
+        <source>Layer Tools</source>
+        <translation>Ferramentas de Camada</translation>
+    </message>
+    <message>
+        <location filename="dsg_tools.py" line="204"/>
+        <source>Complex Tools</source>
+        <translation>Ferramentas de Complexos</translation>
+    </message>
+    <message>
+        <location filename="dsg_tools.py" line="207"/>
+        <source>About</source>
+        <translation>Sobre</translation>
+    </message>
+    <message>
+        <location filename="dsg_tools.py" line="222"/>
+        <source>Create Spatialite</source>
+        <translation>Criar Spatialite</translation>
+    </message>
+    <message>
+        <location filename="dsg_tools.py" line="234"/>
+        <source>Create PostGIS</source>
+        <translation>Criar Postgis</translation>
+    </message>
+    <message>
+        <location filename="dsg_tools.py" line="245"/>
+        <source>Load by Category</source>
+        <translation>Carregar por Categoria</translation>
+    </message>
+    <message>
+        <location filename="dsg_tools.py" line="257"/>
+        <source>Load by Class</source>
+        <translation>Carregar por Classe</translation>
+    </message>
+    <message>
+        <location filename="dsg_tools.py" line="268"/>
+        <source>Create Frame</source>
+        <translation>Criar Moldura</translation>
+    </message>
+    <message>
+        <location filename="dsg_tools.py" line="279"/>
+        <source>Create Complex</source>
+        <translation>Criar Complexo</translation>
+    </message>
+    <message>
+        <location filename="dsg_tools.py" line="291"/>
+        <source>Inspect Complex</source>
+        <translation>Inspecionar Complexo</translation>
+    </message>
+</context>
+</TS>
