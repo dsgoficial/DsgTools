0366c1183fde4b5c469c5ac31c3f9fc7a826e1a7
lcoandrade <lcoandrade@gmail.com>
2014-11-10 17:48:20 -0200

    Still testing alternatives

--- ComplexTools/createComplex.py
+++ ComplexTools/createComplex.py
@@ -27,6 +27,8 @@ from qgis.core import *
 from qgis.gui import *
 from PyQt4.QtXml import *
 
+from PyQt4.QtSql import QSqlQueryModel,QSqlDatabase,QSqlQuery
+
 from ui_createComplex import Ui_Dialog
 
 class CreateComplexDialog(QDialog, Ui_Dialog):
@@ -38,8 +40,52 @@ class CreateComplexDialog(QDialog, Ui_Dialog):
         
         self.iface = iface
         
+        QObject.connect(self.filePushButton, SIGNAL(("clicked()")), self.setFile)
+        QObject.connect(self.fileLineEdit, SIGNAL(("editingFinished()")), self.loadDb)
+        QObject.connect(self.comboBox, SIGNAL("currentIndexChanged (int)"), self.updateTableView)
+        
+        QObject.connect(self.selectAllButton, SIGNAL(("clicked()")), self.selectAllFeatures)
+        QObject.connect(self.selectOneButton, SIGNAL(("clicked()")), self.selectOneFeature)
+        QObject.connect(self.deselectOneButton, SIGNAL(("clicked()")), self.deselectOneFeature)
+        QObject.connect(self.deselectAllButton, SIGNAL(("clicked()")), self.deselectAllFeatures)
+        
         self.populateSelectedFeaturesWidget()
         
+    def setFile(self):
+        fd = QFileDialog()
+        self.filename = fd.getOpenFileName(filter='*.sqlite')
+        if self.filename <> "":
+            self.fileLineEdit.setText(self.filename)
+            
+        self.loadDb()
+
+    def loadDb(self):
+        self.filename = self.fileLineEdit.text()
+        
+        self.db = QSqlDatabase.addDatabase("QSQLITE")
+        self.db.setDatabaseName(self.filename)
+        self.db.open()
+        
+        self.populateComboBox()
+        
+    def updateTableView(self):
+        table = self.comboBox.currentText()
+        
+        projectModel = QSqlQueryModel()
+        projectModel.setQuery("select * from "+table,self.db)
+        
+        self.tableView.setModel(projectModel)
+        self.tableView.show()
+        
+    def populateComboBox(self):
+        self.comboBox.clear()
+        
+        query = QSqlQuery("SELECT name FROM sqlite_master WHERE type='table'", self.db)
+        while query.next():
+            name = query.value(0)
+            if 'Complexo' in name:
+                self.comboBox.addItem(query.value(0))
+
     def populateSelectedFeaturesWidget(self):
         self.layers = self.iface.mapCanvas().layers()
         for layer in self.layers:
@@ -51,3 +97,16 @@ class CreateComplexDialog(QDialog, Ui_Dialog):
             for feature in selectedFeatures:
                 featureItem = QTreeWidgetItem(item)
                 featureItem.setText(0,str(feature.id()))
+                
+    def selectAllFeatures(self):
+        pass
+    
+    def selectOneFeature(self):
+        pass
+
+    def deselectOneFeature(self):
+        pass
+
+    def deselectAllFeatures(self):
+        pass
+    
