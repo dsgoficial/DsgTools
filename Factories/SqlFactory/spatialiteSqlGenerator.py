4d002476b0200e42c8e305e3af9eb52d5da6d3b6
phborba <philipeborba@hotmail.com>
2014-12-29 15:13:42 -0400

    sql create frame bug fix

--- Factories/SqlFactory/spatialiteSqlGenerator.py
+++ Factories/SqlFactory/spatialiteSqlGenerator.py
@@ -69,8 +69,8 @@ class SpatialiteSqlGenerator(SqlGenerator):
     def getCreateDatabase(self, name):
         return None
 
-    def insertFrameIntoTable(self, wkb):
-        sql = "INSERT INTO aux_moldura_a(geom) VALUES(ST_GeomFromText("+wkt+"))"
+    def insertFrameIntoTable(self, wkt):
+        sql = "INSERT INTO aux_moldura_a(GEOMETRY) VALUES(geomfromtext("+wkt+"))"
         return sql
 
     def getElementCountFromLayer(self, layer):
