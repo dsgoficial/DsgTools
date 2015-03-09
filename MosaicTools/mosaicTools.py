6e918c1a5e7a5f7641e42d907994ce3c3f8d17f2
Luiz Andrade <lcoandrade@gmail.com>
2015-03-09 12:11:38 -0300

    Insertion of the image stretching method

--- MosaicTools/mosaicTools.py
+++ MosaicTools/mosaicTools.py
@@ -63,4 +63,75 @@ class MosaicTools(QDialog, Ui_Dialog):
     def on_outputFolderButton_clicked(self):
         print "outputFolderButton"
     
+    #Aplica realce de contraste, reprojecao e troca o tipo de numero utilizando a GDAL. 
+    def stretchImage(inFile, outFile, percent, zone, bands, epsg=4674, maxOutValue=254, minOutValue=0):
+        """Method that applies a specific histogram stretching to a group of images.
+            The method also performs a conversion changing the raster type.
+        """
+
+        #Open image
+        imgIn=gdal.Open(inFile)
+        if not imgIn:
+            QMessageBox.critical(self.iface.mainWindow(), self.tr("Critical!"), self.tr("Failed to open input file."))
+            return
+        
+        #Defining the output driver
+        outDriver=imgIn.GetDriver()
+        createOptions=['PHOTOMETRIC=RGB', 'ALPHA=NO']
+        
+        #creating temp file for contrast stretch
+        outFileTmp=outFile+'tmp'
+        
+        #Creating a temp image, with the same input parameters, to store the converted input image to 8 bits
+        imgOut=outDriver.Create(outFileTmp,imgIn.RasterXSize, imgIn.RasterYSize, len(bands), gdal.GDT_Byte, options = createOptions)
+        imgOut.SetProjection(imgIn.GetProjection())
+        imgOut.SetGeoTransform(imgIn.GetGeoTransform())
+        
+        #Linear stretching
+        topPercent=1-percent/2
+        bottomPercent=percent/2
+        outBandNumber=1
+        for bandNumber in bands:
+            
+            b1=imgIn.GetRasterBand(bandNumber+1)
+            matrix=b1.ReadAsArray()
+            arr=numpy.array(matrix)
+            
+            minValue, maxValue = numpy.percentile(matrix, [bottomPercent*100., topPercent*100.])
+            print minValue, maxValue
     
+            #Transformation parameters
+            #Rouding the values out of bounds
+            numpy.putmask(arr, arr>maxValue,maxValue)
+            numpy.putmask(arr, arr<minValue,minValue)
+            
+            #The maxOutValue and the minOutValue must be set according to the convertion that will be applied (e.g. 8 bits, 16 bits, 32 bits)
+            a=(maxOutValue-minOutValue)/(maxValue-minValue)
+            newArr=numpy.floor((arr-minValue)*a+minOutValue)
+            
+            outB=imgOut.GetRasterBand(outBandNumber)
+            outBandNumber+=1
+            outB.WriteArray(newArr)
+            outB.FlushCache()
+              
+            print "Band", bandNumber, ",", percentValue, ",",
+        
+        #creating final image for reprojection
+        outRasterSRS = osr.SpatialReference()
+        outRasterSRS.ImportFromEPSG(epsg)
+        
+        #this code uses virtual raster to compute the parameters of the output image
+        vrt = gdal.AutoCreateWarpedVRT(imgOut, None, outRasterSRS.ExportToWkt(), gdal.GRA_NearestNeighbour,  0.0)
+        imgWGS=outDriver.CreateCopy(outFile, vrt, options = createOptions)
+        
+        #Checking if the output file was created with success
+        if os.path.exists(outFile):
+            QMessageBox.information(self.iface.mainWindow(), self.tr("Success!"), self.tr("File successfully created: ")+outFile)
+        
+        #Deleting the objects
+        del imgWGS
+        del imgOut    
+        del imgIn
+        
+        #Unlinking the temp file
+        os.unlink(outFileTmp)
