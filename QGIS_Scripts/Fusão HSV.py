8e8973895f8567b6a8247f59aead03454fe955be
lcoandrade <lcoandrade@gmail.com>
2015-06-08 12:17:55 -0300

    Updates...

--- QGIS_Scripts/Fusão HSV.py
+++ QGIS_Scripts/Fusão HSV.py
@@ -30,6 +30,7 @@ from osgeo import gdal, osr
 import sys
 import colorsys
 import numpy
+import struct
 
 
 class RasterProcess():
@@ -53,7 +54,7 @@ class RasterProcess():
             band = raster.GetRasterBand(bandnumber)
         except RuntimeError, e:
             # for example, try GetRasterBand(10)
-            print 'Band ( %i ) not found' % band_num
+            print 'Band ( %i ) not found' % bandnumber
             print e
             sys.exit(1)
 
@@ -76,6 +77,13 @@ class RasterProcess():
         return targetSR
 
     def createRasterFromRGBbands(self, srcraster, red, green, blue, destfile):
+        outRaster = self.createRaster(srcraster, destfile)
+        
+        outRaster.GetRasterBand(1).WriteArray(red)
+        outRaster.GetRasterBand(2).WriteArray(green)
+        outRaster.GetRasterBand(3).WriteArray(blue)
+
+    def createRaster(self, srcraster, destfile):
         cols = srcraster.RasterXSize
         rows = srcraster.RasterYSize
 
@@ -85,35 +93,77 @@ class RasterProcess():
 
         driver = gdal.GetDriverByName('GTiff')
 
-        outRaster = driver.Create(destfile, cols, rows, 3, gdal.GDT_Byte)
+        outRaster = driver.Create(destfile, cols, rows, 3, gdal.GDT_Float32)
         outRaster.SetGeoTransform((xOrigin, pixelWidth, 0, yOrigin, 0, pixelHeight))
-
-        outRaster.GetRasterBand(1).WriteArray(red)
-        outRaster.GetRasterBand(2).WriteArray(green)
-        outRaster.GetRasterBand(3).WriteArray(blue)
-
         outRaster.SetProjection(targetSR.ExportToWkt())
+        
+        return outRaster
 
     def pansharpenImage(self, rgbfile, panfile, destfile):
         rgb = self.openRaster(rgbfile)
-        red = self.getBandAsArray(rgb, 1)
-        green = self.getBandAsArray(rgb, 2)
-        blue = self.getBandAsArray(rgb, 3)
+        red = rgb.GetRasterBand(1)
+        green = rgb.GetRasterBand(2)
+        blue = rgb.GetRasterBand(3)
 
         panraster = self.openRaster(panfile)
-        pan = self.getBandAsArray(panraster, 1)
-
+        pan = panraster.GetRasterBand(1)
+        
         rgb_to_hsv = numpy.vectorize(colorsys.rgb_to_hsv)
         hsv_to_rgb = numpy.vectorize(colorsys.hsv_to_rgb)
 
-        h, s, v = rgb_to_hsv(red, green, blue)
-        r, g, b = hsv_to_rgb(h, s, pan)
+        outRaster = self.createRaster(rgb, destfile)
+        outR = outRaster.GetRasterBand(1)
+        outG = outRaster.GetRasterBand(2)
+        outB = outRaster.GetRasterBand(3)
+        
+        sizeX = pan.XSize
+        sizeY = pan.YSize
+        for row in range(sizeY):
+            redblock = self.readBlock(red, sizeX, 1, row, gdal.GDT_Byte)
+            greenblock = self.readBlock(green, sizeX, 1, row, gdal.GDT_Byte)
+            blueblock = self.readBlock(blue, sizeX, 1, row, gdal.GDT_Byte)
+            
+            panblock = self.readBlock(pan, sizeX, 1, row, gdal.GDT_Float32)
+            
+            h, s, v = rgb_to_hsv(redblock, greenblock, blueblock)
+            r, g, b = hsv_to_rgb(h, s, panblock)
+            
+            self.writeBlock(outR, r, sizeX, 1, row, gdal.GDT_Float32)
+            self.writeBlock(outG, g, sizeX, 1, row, gdal.GDT_Float32)
+            self.writeBlock(outB, b, sizeX, 1, row, gdal.GDT_Float32)
 
-        self.createRasterFromRGBbands(rgb, r, g, b, destfile)
 
         rgb = None
         panraster = None
+        outRaster = None
+        
+    def normalize(self, arr):
+        ''' Function to normalize an input array to 0-1 '''
+        arr_min = arr.min()
+        arr_max = arr.max()
+        return [(arr - arr_min) / (arr_max - arr_min)]*255
+    
+    def readBlock(self, band, sizeX, sizeY = 1, offsetY = 0, pixelType = gdal.GDT_Byte):
+        numpytype = self.getNumpyType(pixelType)
 
+        bandscanline =band.ReadRaster( 0, offsetY, sizeX, sizeY, sizeX, sizeY, pixelType )
+        pixelArray=numpy.fromstring(bandscanline, dtype=numpytype)
+        
+        return pixelArray
+    
+    def getNumpyType(self, pixelType = gdal.GDT_Byte):
+        numpytype = None
+        if pixelType == gdal.GDT_Byte:
+            numpytype = numpy.ubyte
+        elif pixelType == gdal.GDT_Float32:
+            numpytype = numpy.float32
+            
+        return numpytype
+    
+    def writeBlock(self, band, block, sizeX, sizeY = 1, offsetY = 0, pixelType = gdal.GDT_Byte):
+        numpytype = self.getNumpyType(pixelType)
+
+        band.WriteRaster(0, offsetY, sizeX, sizeY, block.astype(numpytype).tostring())
 
 obj = RasterProcess()
 obj.pansharpenImage(Arquivo_RGB,
