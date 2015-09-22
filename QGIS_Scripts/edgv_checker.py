fef5d498f3a013f14560a4f0e8edd05fb03fb7dc
Luiz Andrade <lcoandrade@gmail.com>
2015-09-22 20:25:53 -0300

    Script to check if a sqlite file is a EDGV database.

---
+++ QGIS_Scripts/edgv_checker.py
@@ -0,0 +1,36 @@
+##DSG=group
+##Pasta_de_Busca=folder
+##Relatorio=output table
+
+from processing.core.GeoAlgorithmExecutionException import GeoAlgorithmExecutionException
+import csv, os
+from osgeo import ogr
+
+def edgv_checker(folder, output):
+    csvfile = open(output, 'wb')
+    outwriter = csv.writer(csvfile)
+    outwriter.writerow(['arquivo', 'edgv'])
+
+    for root, dirs, files in os.walk(folder):
+        count = 0
+        size = len(files)
+        p = 0
+        progress.setPercentage(p)
+        for file in files:
+            if '.sqlite' not in file:
+                continue
+            file = os.path.join(root,file)
+            ogrSrc = ogr.Open(file)
+            isEdgv = False
+            if ogrSrc:
+                layer = ogrSrc.GetLayerByName('cb_adm_area_pub_civil_a')
+                isEdgv = True if layer else False
+            outwriter.writerow([file, isEdgv])
+
+            if int(float(count)/size*100) != p:
+                p = int(float(count)/size*100)
+                progress.setPercentage(p)
+
+    csvfile.close()
+
+edgv_checker(Pasta_de_Busca, Relatorio)
