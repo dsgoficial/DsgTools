9638b0abee91872e34c46104eb6eeefc81767461
lcoandrade <lcoandrade@gmail.com>
2015-09-29 10:29:27 -0300

    Bigger and better script...

--- QGIS_Scripts/virtual_raster.py
+++ QGIS_Scripts/virtual_raster.py
@@ -5,67 +5,118 @@
 
 import processing
 from processing.core.GeoAlgorithmExecutionException import GeoAlgorithmExecutionException
-from qgis.core import QgsRasterLayer, QgsSpatialIndex, QgsFeatureRequest, QgsCoordinateTransform
+from qgis.core import QgsVectorLayer, QgsRasterLayer, QgsSpatialIndex, QgsFeatureRequest, QgsCoordinateTransform, QgsFeature
 import os
 
-def reprojectLayer(inventario, frame):
+#script methods
+def createReprojectedLayer(layer, crs):
+    temp = QgsVectorLayer('%s?crs=%s'% ('Multipolygon', crs.authid()), 'temp', 'memory')
+    if not layer.isValid():
+        raise GeoAlgorithmExecutionException('Problema ao criar camada reprojetada!')
+        return None
+    
+    provider = temp.dataProvider()
+    provider.addAttributes(layer.dataProvider().fields())
+    temp.updateFields()
+    
+    coordinateTransformer = QgsCoordinateTransform(layer.crs(), crs)
+    features = []
+    for feature in layer.getFeatures():
+        feat = QgsFeature(feature)
+        geom = feat.geometry()
+        geom.transform(coordinateTransformer)
+        feat.setGeometry(geom)
+        features.append(feat)
+        
+    provider.addFeatures(features)
+    
+    return temp
+
+def reprojectLayer(fromLayer, toLayer):
     ret = []
-    coordinateTransformer = QgsCoordinateTransform(inventario.crs(), frame.crs())
-    for feat in inventario.getFeatures():
+    coordinateTransformer = QgsCoordinateTransform(fromLayer.crs(), toLayer.crs())
+    for feat in fromLayer.getFeatures():
         f = feat
         f.geometry().transform(coordinateTransformer)
         ret.append(f)
     return ret
 
+def populateIndex(idx, layer):
+    for feat in layer.getFeatures():
+        idx.insertFeature(feat)
+        
+def getCandidates(idx, layer, bbox):
+    ids = idx.intersects(bbox)
+    candidates = []
+    for id in ids:
+        candidates.append(layer.getFeatures(QgsFeatureRequest().setFilterFid(id)).next())
+    return candidates
+    
+def makeVrtDict(candidates, camada):
+    vrt = dict()
+    for candidate in candidates:
+        map_index = candidate['map_index']
+        vrt[map_index] = []
+        for feat in camada.getFeatures():
+            if candidate.geometry().intersects(feat.geometry()):
+                vrt[map_index].append(feat)
+    return vrt            
+            
+def createVrt(vrt):
+    count = 0
+    size = len(vrt.keys())
+    p = 0
+    progress.setPercentage(p)    
+    for key in vrt.keys():
+        vrtfilename = os.path.join(Pasta, key+'.vrt')
+        features = vrt[key]
+        rasterList = []
+        for feat in features:
+            filename = feat['fileName']
+            raster = QgsRasterLayer(filename, filename)
+            rasterList.append(raster)
+            ovr = filename+'.ovr'
+            if not os.path.isfile(ovr):
+                progress.setText('Fazendo Pirâmides...')
+                #('gdalogr:overviews', input, levels=8, clean=False, resampling_method=0(nearest), format=1(Gtiff .ovr))
+                processing.runalg('gdalogr:overviews', raster, 8, True, 0, 1)
+        
+        if int(float(count)/size*100) != p:
+            p = int(float(count)/size*100)
+            progress.setPercentage(p)    
+    
+        progress.setText('Fazendo raster virtual...')
+        processing.runalg('gdalogr:buildvirtualraster', rasterList, 0, False, False, vrtfilename)
+#end of script methods
+        
+##Making the actual work
+#Camada de inventario
 inventario = processing.getObject(Inventario)
+
+#Camada de moldura
 frame = processing.getObject(Moldura)
+
+#Indice espacial
 frameidx = QgsSpatialIndex()
 
-#reprojecting inventory layer
-reprojectedFeatures = reprojectLayer(inventario, frame)
-
 #Populating a spatial index
-for inom in frame.getFeatures():
-    frameidx.insertFeature(inom)
+populateIndex(frameidx, frame)
 
-ids = frameidx.intersects(inventario.extent())
-candidates = []
-for id in ids:
-    candidates.append(frame.getFeatures(QgsFeatureRequest().setFilterFid(id)).next())
+#checking crs's, if different, reproject
+destCrs = frame.crs()
+srcCrs = inventario.crs()
+if srcCrs.authid() != destCrs.authid():
+    progress.setText('CRS diferentes, reprojetando...')
+    camada = createReprojectedLayer(inventario, frame.crs())
+else:
+    camada = inventario
     
-vrt = dict()
-for candidate in candidates:
-    map_index = candidate['map_index']
-    print map_index
-    vrt[map_index] = []
-    for feat in reprojectedFeatures:
-        if candidate.geometry().intersects(feat.geometry()):
-            vrt[map_index].append(feat)
-            
-count = 0
-size = len(vrt.keys())
-p = 0
-progress.setPercentage(p)    
-for key in vrt.keys():
-    vrtfilename = os.path.join(Pasta, key+'.vrt')
-    print vrtfilename
-    features = vrt[key]
+#Obtaining candidates
+candidates = getCandidates(frameidx, frame, camada.extent())
 
-    rasterList = []
-    
-    for feat in features:
-        filename = feat['fileName']
-        raster = QgsRasterLayer(filename, filename)
-        rasterList.append(raster)
-        ovr = filename+'.ovr'
-        if not os.path.isfile(ovr):
-            progress.setText('Fazendo Pirâmides...')
-            #('gdalogr:overviews', input, levels=8, clean=False, resampling_method=0(nearest), format=1(Gtiff .ovr))
-            processing.runalg('gdalogr:overviews', raster, 8, False, 0, 1)
-    
-    if int(float(count)/size*100) != p:
-        p = int(float(count)/size*100)
-        progress.setPercentage(p)    
+#making vrt dict
+vrt = makeVrtDict(candidates, camada)
 
-    progress.setText('Fazendo raster virtual...')
-    processing.runalg('gdalogr:buildvirtualraster', rasterList, 0, False, False, vrtfilename)
+#creating vrt files
+createVrt(vrt)
+##ending the actual work
