d1cfe03ce89134b1be7d1c06d54829f0ba38e463
phborba <philipeborba@hotmail.com>
2016-07-31 19:52:34 -0300

    beginning dbCustomization

---
+++ Factories/DbCustomizationFactory/dbCustomizationFactory.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ DsgTools
+                                 A QGIS plugin
+ Brazilian Army Cartographic Production Tools
+                              -------------------
+        begin                : 2016-07-31
+        git sha              : $Format:%H$
+        copyright            : (C) 2016 by Philipe Borba - Cartographic Engineer @ Brazilian Army
+        email                : borba@dsg.eb.mil.br
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+#DsgTools imports
+from DsgTools.Factories.CustomizationFactory.classCustomization import ClassCustomization
+from DsgTools.Factories.CustomizationFactory.codeNameCustomization import CodeNameCustomization
+from DsgTools.Factories.CustomizationFactory.attributeCustomization import AttributeCustomization
+from DsgTools.Factories.CustomizationFactory.domainCustomization import DomainCustomization
+
+class CustomizationFactory:
+    def createCustomization(self, type, validatedJSONDict):
+        if type == 'class':
+            return ClassCustomization(validatedJSONDict)
+        if type == 'codeName':
+            return CodeNameCustomization(validatedJSONDict)
+        if type == 'attribute':
+            return AttributeCustomization(validatedJSONDict)
+        if type == 'domain':
+            return DomainCustomization(validatedJSONDict)
---
+++ PostgisCustomization/CustomJSONValidator/customJSONValidator.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ DsgTools
+                                 A QGIS plugin
+ Brazilian Army Cartographic Production Tools
+                              -------------------
+        begin                : 2016-07-31
+        git sha              : $Format:%H$
+        copyright            : (C) 2016 by Philipe Borba - Cartographic Engineer @ Brazilian Army
+        email                : borba@dsg.eb.mil.br
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+#Qt Imports
+import json
+#DsgTools Imports
+
+class CustomJSONValidator(QObject):
+    def __init__(self, jsonFile):
+        super(CustomJSONValidator,self).__init__()
+        self.jsonFile = jsonFile
+    
+    def parseJson(self):
+        pass
+    
+    def validateTags(self):
+        pass
+    
+    def validateDsgToolsClassNames(self):
+        pass
+    
+    def validateGeometricPrimitives(self):
+        pass
+    
+    def validate(self):
+        pass
