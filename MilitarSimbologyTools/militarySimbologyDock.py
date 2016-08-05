5af38ad787047f388725c2f8117f6be9c9887bd4
phborba <philipeborba@hotmail.com>
2016-08-05 11:31:43 -0300

    military symbology prototype

---
+++ MilitarSimbologyTools/militarySimbologyDock.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ DsgTools
+                                 A QGIS plugin
+ Brazilian Army Cartographic Production Tools
+                              -------------------
+        begin                : 2016-08-05
+        git sha              : $Format:%H$
+        copyright            : (C) 2016 by Philipe Borba - Cartographic Engineer @ Brazilian Army
+        email                : borba@dsg.eb.mil.br
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+import os
+
+# Qt imports
+from PyQt4 import QtGui, uic
+from PyQt4.QtCore import pyqtSlot
+from PyQt4.QtCore import pyqtSlot, pyqtSignal
+
+# QGIS imports
+from qgis.core import QgsMapLayer, QgsGeometry, QgsMapLayerRegistry, QgsProject, QgsLayerTreeLayer, QgsFeature, QgsMessageLog, QgsCoordinateTransform, QgsCoordinateReferenceSystem
+from qgis.gui import QgsMessageBar
+import qgis as qgis
+
+#DsgTools imports
+from DsgTools.ProductionTools.field_setup import FieldSetup
+from DsgTools.Factories.DbFactory.dbFactory import DbFactory
+from DsgTools.Factories.LayerFactory.layerFactory import LayerFactory
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'militarySimbologyDock.ui'))
+
+class MilitarySimbologyDock(QtGui.QDockWidget, FORM_CLASS):
+    def __init__(self, iface, codeList, parent = None):
+        """Constructor."""
+        super(self.__class__, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
