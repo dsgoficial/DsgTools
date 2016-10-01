6c71258193a71f595c7817cc815308ff97d3d99e
dmcarvalho <moreira.geo@gmail.com>
2016-10-01 20:22:29 -0300

    CONTOUR_TOOL - Adds a dialog to put an initial value when the first feature attribute is null

---
+++ VectorTools/contour_value.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ DsgTools
+                                 A QGIS plugin
+ Brazilian Army Cartographic Production Tools
+                             -------------------
+        begin                : 2015-11-10
+        git sha              : $Format:%H$
+        copyright            : (C) 2015 by Brazilian Army - Geographic Service Bureau
+        email                : suporte.dsgtools@dsg.eb.mil.br
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+import os
+
+# Qt imports
+from PyQt4 import QtGui, uic
+from PyQt4.QtCore import pyqtSlot
+from PyQt4.QtGui import QIntValidator
+
+#DSGTools imports
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'contour_value.ui'))
+
+class ContourValue(QtGui.QDialog, FORM_CLASS):
+    def __init__(self, contour_tool, parent = None):
+        """
+        Constructor"""
+        super(ContourValue, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
+        self.value_line_edit.setValidator(QIntValidator(0, 1000000))
+        self.contour_tool = contour_tool
+        
+    @pyqtSlot(bool)
+    def on_cancel_push_button_clicked(self):
+        '''
+        Closes the dialog
+        '''
+        self.done(0)
+        
+    @pyqtSlot()    
+    def on_ok_push_button_clicked(self): 
+        value = self.value_line_edit.text()
+        self.contour_tool.setFirstValue(int(value))
+        self.done(1)
+        
