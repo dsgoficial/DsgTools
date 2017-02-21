3e2763ce12c60501fb5a2bcbcf27d3b6e326bf65
Luiz Andrade <lcoandrade@gmail.com>
2017-02-21 16:48:03 -0300

    Improvement in the calc contour...

--- ProductionTools/ContourTool/contour_tool.py
+++ ProductionTools/ContourTool/contour_tool.py
@@ -22,19 +22,30 @@
 """
 
 from qgis.core import QgsFeatureRequest, QgsGeometry, QGis, QgsSpatialIndex, QgsCoordinateTransform
+from DsgTools.ProductionTools.ContourTool.contour_value import ContourValue
 
 class ContourTool():
     def updateReference(self, referenceLayer):
+        '''
+        Updates the reference layer and updates the spatial index
+        '''
+        self.first_value = None
         self.reference = referenceLayer
         self.populateIndex()
 
     def populateIndex(self):
+        '''
+        Populates the spatial index
+        '''
         #spatial index
         self.index = QgsSpatialIndex()
         for feat in self.reference.getFeatures():
             self.index.insertFeature(feat)
             
     def getCandidates(self, bbox):
+        '''
+        Gets candidates using the spatial index to speedup the process
+        '''
         #features that might satisfy the query
         ids = self.index.intersects(bbox)
         candidates = []
@@ -43,6 +54,9 @@ class ContourTool():
         return candidates            
             
     def getFeatures(self, geom):
+        '''
+        Gets the features that intersect geom to be updated
+        '''
         #features that satisfy the query
         ret = []
         
@@ -56,9 +70,15 @@ class ContourTool():
         return ret
     
     def getKey(self, item):
+        '''
+        Gets the key
+        '''
         return item[0]
                 
     def sortFeatures(self, geom, features):
+        '''
+        Sorts features according to the distance
+        '''
         #sorting by distance
         distances = []
         
@@ -76,12 +96,21 @@ class ContourTool():
         return ordered
 
     def reproject(self, geom, canvasCrs):
+        '''
+        Reprojects geom to the reference layer crs
+        '''
         destCrs = self.reference.crs()
         if canvasCrs.authid() != destCrs.authid():
             coordinateTransformer = QgsCoordinateTransform(canvasCrs, destCrs)
             geom.transform(coordinateTransformer)
     
+    def setFirstValue(self, value):
+        self.first_value = value
+
     def assignValues(self, attribute, pace, geom, canvasCrs):
+        '''
+        Assigns attribute values to all features that intersect geom.
+        '''
         self.reproject(geom, canvasCrs)
         features = self.getFeatures(geom)
         if len(features) == 0:
@@ -93,11 +122,22 @@ class ContourTool():
 
         #the first feature must have the initial value already assigned
         first_feature = ordered[0][1]
-        #getting the initial value
-        first_value = first_feature.attribute(attribute)
-
         #getting the filed index that must be updated
         fieldIndex = self.reference.fieldNameIndex(attribute)
+        #getting the initial value
+        first_value = first_feature.attribute(attribute)
+        if not first_value:
+            first_value_dlg = ContourValue(self)
+            retorno = first_value_dlg.exec_()
+            if self.first_value:
+                id = first_feature.id()
+                first_value = self.first_value
+                if not self.reference.changeAttributeValue(id, fieldIndex, self.first_value):
+                    return 0
+            else:
+                return -3
+            self.first_value = None
+                
 
         self.reference.startEditing()
         for i in range(1, len(ordered)):
@@ -110,5 +150,3 @@ class ContourTool():
             #actual update in the layer
             if not self.reference.changeAttributeValue(id, fieldIndex, value):return 0
         return 1
-            
-        
