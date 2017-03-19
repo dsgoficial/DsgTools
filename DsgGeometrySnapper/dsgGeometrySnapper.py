7ed84284ebc511e39b76370da8d14d00f1aa69e4
lcoandrade <lcoandrade@gmail.com>
2017-03-19 02:21:40 -0300

    dsggeometrysnapper test...

--- DsgGeometrySnapper/dsgGeometrySnapper.py
+++ DsgGeometrySnapper/dsgGeometrySnapper.py
@@ -24,7 +24,7 @@ import sys
 
 from PyQt4.QtCore import QObject, QtConcurrent
 
-from qgis.core import QgsFeatureRequest, QgsAttributeList, QgsSpatialIndex, QgsGeometry, QgsPointV2, QgsGeometryUtils, QgsFeatureRequest, QgsFeatureIterator, QgsFeature, QgsVertexId, QgsPointV2, QgsCurvePolygonV2, QgsVertexId
+from qgis.core import QgsFeatureRequest, QgsAttributeList, QgsSpatialIndex, QgsGeometry, QgsPointV2, QgsGeometryUtils, QgsFeatureRequest, QgsFeatureIterator, QgsFeature, QgsVertexId, QgsPointV2, QgsCurvePolygonV2, QgsVertexId, QgsVectorLayer
 
 from DsgTools.DsgGeometrySnapper.dsgSnapIndex import DsgSnapIndex
 
@@ -189,16 +189,16 @@ class DsgGeometrySnapper(QObject):
         return QgsGeometry(subjGeom)
 
 if name == '__main__':
-    rl = QgsVectorLayer(QStringLiteral("Polygon"), QStringLiteral("x"), QStringLiteral("memory"));
+    rl = QgsVectorLayer("Polygon", "x", "memory");
     ff = QgsFeature()
-    refGeom = QgsGeometry.fromWkt(QStringLiteral("Polygon((0 0, 10 0, 10 10, 0 10, 0 0))"));
+    refGeom = QgsGeometry.fromWkt("Polygon((0 0, 10 0, 10 10, 0 10, 0 0))");
     ff.setGeometry(refGeom)
     flist = []
     flist.append(ff)
     rl.dataProvider().addFeatures(flist)
 
-    polygonGeom = QgsGeometry::fromWkt(QStringLiteral("Polygon((0.1 -0.1, 10.1 0, 9.9 10.1, 0 10, 0.1 -0.1))"));
-    snapper = QgsGeometrySnapper(rl)
+    polygonGeom = QgsGeometry.fromWkt("Polygon((0.1 -0.1, 10.1 0, 9.9 10.1, 0 10, 0.1 -0.1))");
+    snapper = DsgGeometrySnapper(rl)
     result = snapper.snapGeometry(polygonGeom, 1)
     print 'saida' result.exportToWkt()
     print 'esperado', "Polygon ((0 0, 10 0, 10 10, 0 10, 0 0))"
