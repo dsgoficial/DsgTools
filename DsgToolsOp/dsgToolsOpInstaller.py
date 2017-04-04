d1e92293fe1f2d9968b104c7d92fe222463553ab
phborba <philipeborba@hotmail.com>
2017-04-04 20:42:00 -0300

    delete aux now works

--- DsgToolsOp/dsgToolsOpInstaller.py
+++ DsgToolsOp/dsgToolsOpInstaller.py
@@ -43,11 +43,7 @@ class DsgToolsOpInstaller(QObject):
     def deleteAuxFolder(self):
         currentPath = os.path.dirname(__file__)
         top = os.path.join(currentPath, 'aux')
-        for root, dirs, files in os.walk(top, topdown=False):
-            for name in files:
-                os.remove(os.path.join(root, name))
-            for name in dirs:
-                os.rmdir(os.path.join(root, name))
+        shutil.rmtree(top, ignore_errors=False)
 
     def installDsgToolsOp(self, fullZipPath):
         try:
