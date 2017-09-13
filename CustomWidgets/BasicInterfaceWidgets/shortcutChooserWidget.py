157ed3946ec042652ec0a33e4a1025ac8d67858f
phborba <philipeborba@gmail.com>
2017-09-12 22:40:09 -0300

    first commit on shortcutChooser

---
+++ CustomWidgets/BasicInterfaceWidgets/shortcutChooserWidget.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ DsgTools
+                                 A QGIS plugin
+ Brazilian Army Cartographic Production Tools
+                              -------------------
+        begin                : 2017-09-12
+        git sha              : $Format:%H$
+        copyright            : (C) 2017 by Philipe Borba - Cartographic Engineer @ Brazilian Army
+        email                : borba.philipe@eb.mil.br
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+import os
+# Qt imports
+from PyQt4 import QtGui, uic
+from PyQt4.QtCore import pyqtSlot, pyqtSignal, QSettings, Qt
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'shortcutChooserWidget.ui'))
+
+class ShortcutChooserWidget(QtGui.QWidget, FORM_CLASS):
+    def __init__(self, parent=None):
+        super(ShortcutChooserWidget, self).__init__(parent)
+        self.setupUi(self)
