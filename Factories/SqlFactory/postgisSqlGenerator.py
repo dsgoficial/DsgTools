be347563f45cb0d74c75864d3854a87e02c21ad8
lcoandrade <lcoandrade@gmail.com>
2017-09-27 14:21:47 -0300

    making the query more readable

--- Factories/SqlFactory/postgisSqlGenerator.py
+++ Factories/SqlFactory/postgisSqlGenerator.py
@@ -1504,8 +1504,9 @@ class PostGISSqlGenerator(SqlGenerator):
     def checkCoverageForGaps(self, frameTable, geomColumn):
         frameSchema, frameTable = frameTable.split('.')
         sql = """
-        select (ST_Dump(ST_SymDifference(ST_Union(a.{0}), b.geom))).geom as geom from "{1}"."{2}" as a, 
-        (select ST_Union(geom) as geom from validation.coverage_temp) as b GROUP BY  b.geom
+        select (ST_Dump(ST_SymDifference(a.geom, b.geom))).geom from
+        (select ST_Union({0}) as geom from "{1}"."{2}") as a,
+        (select ST_Union(geom) as geom from validation.coverage_temp) as b
         """.format(geomColumn, frameSchema, frameTable)
         return sql
 
