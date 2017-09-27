06a6047f7b130ba34f2b965fa3b182ad1397dac0
Luiz Andrade <lcoandrade@gmail.com>
2017-09-27 12:02:17 -0300

    multi frame problem solved

--- Factories/SqlFactory/postgisSqlGenerator.py
+++ Factories/SqlFactory/postgisSqlGenerator.py
@@ -1504,8 +1504,8 @@ class PostGISSqlGenerator(SqlGenerator):
     def checkCoverageForGaps(self, frameTable, geomColumn):
         frameSchema, frameTable = frameTable.split('.')
         sql = """
-        select (ST_Dump(ST_SymDifference(a.{0}, b.geom))).geom as geom from "{1}"."{2}" as a, 
-        (select ST_Union(geom) as geom from validation.coverage_temp) as b
+        select (ST_Dump(ST_SymDifference(ST_Union(a.{0}), b.geom))).geom as geom from "{1}"."{2}" as a, 
+        (select ST_Union(geom) as geom from validation.coverage_temp) as b GROUP BY  b.geom
         """.format(geomColumn, frameSchema, frameTable)
         return sql
 
