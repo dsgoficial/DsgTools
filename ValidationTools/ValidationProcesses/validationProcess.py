e01d334006bc7c3876926e2032fd5bdcc138870b
phborba <philipeborba@gmail.com>
2017-09-29 23:38:24 -0300

    removendo cagada

--- ValidationTools/ValidationProcesses/validationProcess.py
+++ ValidationTools/ValidationProcesses/validationProcess.py
@@ -213,7 +213,7 @@ class ValidationProcess(QObject):
                 featureMap[feat.id()] = feat
         return featureMap
     
-    def updateOriginalLayer(self, pgInputLayer, qgisOutputVector, featureList=None, featureTupleList=None, overlayOutput=False):
+    def updateOriginalLayer(self, pgInputLayer, qgisOutputVector, featureList=None, featureTupleList=None):
         """
         Updates the original layer using the grass output layer
         pgInputLyr: postgis input layer
@@ -260,7 +260,7 @@ class ValidationProcess(QObject):
                     newFeat.setAttribute(idx, provider.defaultValue(idx))
                     addList.append(newFeat)
             #in the case we don't find features in the output we should mark them to be removed
-            if len(outFeats) == 0 and not overlayOutput:
+            if len(outFeats) == 0:
                 idsToRemove.append(id)
         #pushing the changes into the edit buffer
         pgInputLayer.addFeatures(addList, True)
