588325d27b4608c07e42006a886470828a6b0ea3
phborba <philipeborba@hotmail.com>
2017-10-17 11:04:43 -0200

    messages and layers loading

--- ValidationTools/ValidationProcesses/overlayElementsWithAreasProcess.py
+++ ValidationTools/ValidationProcesses/overlayElementsWithAreasProcess.py
@@ -50,7 +50,55 @@ class OverlayElementsWithAreasProcess(ValidationProcess):
                 interfaceDict[key] = {self.tr('Category'):cat, self.tr('Layer Name'):lyrName, self.tr('Geometry\nColumn'):geom, self.tr('Geometry\nType'):geomType, self.tr('Layer\nType'):tableType}
             self.opTypeDict = OrderedDict([(self.tr('Overlay and Keep Elements'),-1), (self.tr('Remove outside elements'),0), (self.tr('Remove inside elements'),1)])
             self.parameters = {'Snap': 1.0, 'MinArea': 0.001, 'Overlayer and Layers': OrderedDict({'referenceDictList':overlayInterfaceDict, 'layersDictList':interfaceDict}), 'Overlay Type':deque(self.opTypeDict.keys())}
-        
+
+    def execute(self):
+        """
+        Reimplementation of the execute method from the parent class
+        """
+        QgsMessageLog.logMessage(self.tr('Starting ')+self.getName()+self.tr(' Process.'), "DSG Tools Plugin", QgsMessageLog.CRITICAL)
+        try:
+            self.setStatus(self.tr('Running'), 3) #now I'm running!
+            self.abstractDb.deleteProcessFlags(self.getName()) #erase previous flags
+            overlayerKey, lyrListKeys = self.parameters['Overlayer and Layers']
+            if len(lyrListKeys) == 0:
+                self.setStatus(self.tr('No classes selected!. Nothing to be done.'), 1) #Finished
+                QgsMessageLog.logMessage(self.tr('No classes selected! Nothing to be done.'), "DSG Tools Plugin", QgsMessageLog.CRITICAL)
+                return 1
+            if not overlayerKey:
+                self.setStatus(self.tr('One overlayer must be selected! Stopping.'), 1) #Finished
+                QgsMessageLog.logMessage(self.tr('One reference must be selected! Stopping.'), "DSG Tools Plugin", QgsMessageLog.CRITICAL)
+                return 1
+            error = False
+            overlayer = self.overlayElemDict[overlayerKey]
+            overLyr = self.loadLayerBeforeValidationProcess(overlayer)
+            for lyrKey in lyrListKeys:
+                # preparation
+                cl = self.classesWithElemDict[lyrKey]
+                lyr = self.loadLayerBeforeValidationProcess(cl)                   
+
+                # running the process in the temp table
+                result = self.runProcessinAlg(lyr, overLyr)
+                
+                # storing flags
+                if len(result) > 0:
+                    error = True
+                    recordList = []
+                    for tupple in result:
+                        recordList.append((cl, tupple[0], self.tr('Overlay error.'), tupple[1], cl['geom']))
+                    numberOfProblems = self.addFlag(recordList)
+                    QgsMessageLog.logMessage(str(numberOfProblems) + self.tr(' feature(s) from {0}.{1}').format(cl['tableSchema'], cl['tableName']) + self.tr(' with overlay errors. Check flags.'), "DSG Tools Plugin", QgsMessageLog.CRITICAL)
+                else:
+                    QgsMessageLog.logMessage(self.tr('All features from {0}.{1} overlayed to elements from {2}.{3}.').format(cl['tableSchema'], cl['tableName'], overlayer['tableSchema'], overlayer['tableName']), "DSG Tools Plugin", QgsMessageLog.CRITICAL)
+            if error:
+                self.setStatus(self.tr('There are overlay errors. Check log.'), 4) #Finished with errors
+            else:
+                self.setStatus(self.tr('Overlay process complete.'), 1) #Finished
+            return 1
+        except Exception as e:
+            QgsMessageLog.logMessage(':'.join(e.args), "DSG Tools Plugin", QgsMessageLog.CRITICAL)
+            self.finishedWithError()
+            return 0
+
     def runProcessinAlg(self, layerA, layerB):
         """
         Runs the actual grass process
@@ -87,47 +135,3 @@ class OverlayElementsWithAreasProcess(ValidationProcess):
         #getting error flags
         errorLayer = processing.getObject(ret['error'])
         return self.getProcessingErrors(errorLayer)
-
-    def execute(self):
-        """
-        Reimplementation of the execute method from the parent class
-        """
-        QgsMessageLog.logMessage(self.tr('Starting ')+self.getName()+self.tr(' Process.'), "DSG Tools Plugin", QgsMessageLog.CRITICAL)
-        try:
-            self.setStatus(self.tr('Running'), 3) #now I'm running!
-            self.abstractDb.deleteProcessFlags(self.getName()) #erase previous flags
-            overlayer, lyrList = self.parameters['Overlayer and Layers']
-            overLyrName, overLyrGeometryColumn = overlayer.split(':')
-            overLyr = self.loadLayerBeforeValidationProcess(overLyrName)
-            if len(lyrList) == 0:
-                self.setStatus(self.tr('No classes selected!. Nothing to be done.'), 1) #Finished
-                QgsMessageLog.logMessage(self.tr('No classes selected! Nothing to be done.'), "DSG Tools Plugin", QgsMessageLog.CRITICAL)
-                return 1
-            error = False
-            for lyrAndGeom in lyrList:
-                # preparation
-                cl, geometryColumn = lyrAndGeom.split(':')
-                lyr = self.loadLayerBeforeValidationProcess(cl)                   
-
-                # running the process in the temp table
-                result = self.runProcessinAlg(lyr, overLyr)
-                
-                # storing flags
-                if len(result) > 0:
-                    error = True
-                    recordList = []
-                    for tupple in result:
-                        recordList.append((cl, tupple[0], self.tr('Cleaning error.'), tupple[1], geometryColumn))
-                    numberOfProblems = self.addFlag(recordList)
-                    QgsMessageLog.logMessage(str(numberOfProblems) + self.tr(' feature(s) from ') + cl + self.tr(' with cleaning errors. Check flags.'), "DSG Tools Plugin", QgsMessageLog.CRITICAL)
-                else:
-                    QgsMessageLog.logMessage(self.tr('There are no cleaning errors on ') + classAndGeom['lyrName'] +'.', "DSG Tools Plugin", QgsMessageLog.CRITICAL)
-            if error:
-                self.setStatus(self.tr('There are cleaning errors. Check log.'), 4) #Finished with errors
-            else:
-                self.setStatus(self.tr('There are no cleaning errors.'), 1) #Finished
-            return 1
-        except Exception as e:
-            QgsMessageLog.logMessage(':'.join(e.args), "DSG Tools Plugin", QgsMessageLog.CRITICAL)
-            self.finishedWithError()
-            return 0
