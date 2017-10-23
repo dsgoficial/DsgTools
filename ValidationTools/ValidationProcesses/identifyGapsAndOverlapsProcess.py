84b16ecbbf075bd37e013a2c27a617cab6b4d3f5
phborba <philipeborba@hotmail.com>
2017-10-23 16:23:52 -0200

    time on identify gaps

--- ValidationTools/ValidationProcesses/identifyGapsAndOverlapsProcess.py
+++ ValidationTools/ValidationProcesses/identifyGapsAndOverlapsProcess.py
@@ -50,6 +50,7 @@ class IdentifyGapsAndOverlapsProcess(ValidationProcess):
         Reimplementation of the execute method from the parent class
         """
         QgsMessageLog.logMessage(self.tr('Starting ')+self.getName()+self.tr(' Process.'), "DSG Tools Plugin", QgsMessageLog.CRITICAL)
+        self.startTimeCount()
         try:
             self.setStatus(self.tr('Running'), 3) #now I'm running!
             self.abstractDb.deleteProcessFlags(self.getName()) #erase previous flags
