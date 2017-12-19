db5be3415b22cf9e0ab46692566f1f1ef2f60605
phborba <philipeborba@hotmail.com>
2017-12-19 15:08:13 -0200

    aux layer selector

---
+++ CustomWidgets/AdvancedInterfaceWidgets/auxLayerSelector.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ DsgTools
+                                 A QGIS plugin
+ Brazilian Army Cartographic Production Tools
+                              -------------------
+        begin                : 2017-12-19
+        git sha              : $Format:%H$
+        copyright            : (C) 2017 by Philipe Borba - Cartographic Engineer @ Brazilian Army
+        email                : borba.philipe@eb.mil.br
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+from PyQt4.QtCore import Qt
+from PyQt4.QtGui import QWidget 
+
+from DsgTools.CustomWidgets.BasicInterfaceWidgets.dsgCustomComboBox import DsgCustomComboBox
+
+class AuxLayerSelector(QWidget):
+    def __init__(self, parent=None):
+        super(AuxLayerSelector, self).__init__(parent)
