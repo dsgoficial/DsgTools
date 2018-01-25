a4c17f23d9afd40d062f6fb1552b00c9ac5d1764
phborba <philipeborba@hotmail.com>
2018-01-25 18:03:29 -0200

    minor fix

--- ValidationTools/ValidationProcesses/validationProcess.py
+++ ValidationTools/ValidationProcesses/validationProcess.py
@@ -180,7 +180,7 @@ class ValidationProcess(QObject):
         msg: Status text message
         """
         try:
-            if status not in [0,2]:
+            if status in [1,4]:
                 self.logProcess()
                 if self.logMsg:
                     msg += "\n\n" + self.logMsg
