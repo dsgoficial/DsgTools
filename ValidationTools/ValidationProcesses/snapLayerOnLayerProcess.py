c642a2f66c420ec0a97b01651baafb7ec1ea2559
lcoandrade <lcoandrade@gmail.com>
2018-02-18 08:01:08 -0300

    bug fix on snap layer on layer

--- ValidationTools/ValidationProcesses/snapLayerOnLayerProcess.py
+++ ValidationTools/ValidationProcesses/snapLayerOnLayerProcess.py
@@ -51,6 +51,7 @@ class SnapLayerOnLayerProcess(ValidationProcess):
         Reimplementation of the execute method from the parent class
         """
         QgsMessageLog.logMessage(self.tr('Starting ')+self.getName()+self.tr(' Process.'), "DSG Tools Plugin", QgsMessageLog.CRITICAL)
+        self.startTimeCount()
         try:
             self.setStatus(self.tr('Running'), 3) #now I'm running!
             refKey = self.parameters['Reference and Layers'][0]
