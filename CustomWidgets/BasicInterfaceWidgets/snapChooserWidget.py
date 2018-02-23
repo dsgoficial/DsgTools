23cd7a003168feacf43cc8b1c279c4a6d11f3cc7
phborba <philipeborba@gmail.com>
2018-02-23 00:00:19 -0300

    snap widget

---
+++ CustomWidgets/BasicInterfaceWidgets/snapChooserWidget.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ DsgTools
+                                 A QGIS plugin
+ Brazilian Army Cartographic Production Tools
+                              -------------------
+        begin                : 2016-08-04
+        git sha              : $Format:%H$
+        copyright            : (C) 2016 by Philipe Borba - Cartographic Engineer @ Brazilian Army
+        email                : borba@dsg.eb.mil.br
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+import os, json
+from os.path import expanduser
+
+from qgis.core import QgsMessageLog
+
+# Qt imports
+from PyQt4 import QtGui, uic
+from PyQt4.QtCore import pyqtSlot, Qt, QSettings
+from PyQt4.QtGui import QListWidgetItem, QMessageBox, QMenu, QApplication, QCursor, QFileDialog
+from PyQt4.QtSql import QSqlDatabase,QSqlQuery
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'snapChooserWidget.ui'))
+
+class SnapChooserWidget(QtGui.QWidget, FORM_CLASS):
+    def __init__(self, layerList, parameterDict = {}, parent = None):
+        """Constructor."""
+        super(SnapChooserWidget, self).__init__(parent = parent)
+        self.setupUi(self)
+        self.layerList = layerList
+        self.refresh()
+        self.validKeys = ['layerName', 'snap']
+        widget.setDecimals(20)
+        self.snapDoubleSpinBox.setMaximum(1000000)
+        self.snapDoubleSpinBox.setMinimum(0.00000000000000001)
+        if parameterDict != {}:
+            self.populateInterface(parameterDict)
+    
+    def refresh(self):
+        self.layerComboBox.clear()
+        self.layerComboBox.addItem(self.tr('Select a layer'))
+        self.layerComboBox.addItems(self.layerList)
+    
+    def clearAll(self):
+        """
+        Clears all widget information
+        """
+        self.layerComboBox.clear()
+        self.snapDoubleSpinBox.setValue(1.0)
+    
+    def getParameterDict(self):
+        """
+        Components:
+        parameterDict = {'layerName':--name of the layer--,
+                         'snap': --snap value--}
+        """
+        if not self.validate():
+            raise Exception(self.invalidatedReason())
+        parameterDict = dict()
+        parameterDict['layerName'] = self.layerComboBox.currentText()
+        parameterDict['snap'] = self.snapDoubleSpinBox.value()
+        return parameterDict
+
+    def populateInterface(self, parameterDict):
+        """
+        Populates interface with parameters from parameterDict.
+        """
+        if parameterDict:
+            if not self.validateJson(parameterDict):
+                raise Exception(self.tr('Invalid Snap Chooser Widget json config!'))
+            #set layer combo
+            idx = self.layerComboBox.findText(parameterDict['layerName'], flags = Qt.MatchExactly)
+            self.layerComboBox.setCurrentIndex(idx)
+            #set snap double spin box
+            self.snapDoubleSpinBox.setValue(parameterDict['snap'])
+            
+    
+    def validateJson(self, inputJson):
+        """
+        Validates input json
+        """
+        inputKeys = inputJson.keys()
+        inputKeys.sort()
+        if self.validKeys != inputKeys:
+            return False
+        else:
+            return True
+
+    def validate(self):
+        """
+        Validates fields. Returns True if all information are filled correctly.
+        """
+        if self.layerComboBox.currentIndex() < 1:
+            return False
+        if self.snapDoubleSpinBox.value() <= 0:
+            return False
+        return True
+    
+    def invalidatedReason(self):
+        """
+        Error reason
+        """
+        msg = ''
+        if self.layerComboBox.currentIndex() < 1:
+            msg += self.tr('Invalid layer!\n')
+        if self.snapDoubleSpinBox.value() <= 0:
+            msg += self.tr('Invalid snap value!\n')
+        return msg
