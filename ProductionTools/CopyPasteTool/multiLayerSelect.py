85ddb1c1059f0e4625e5d6ce65ef5c72bdc5af05
phborba <philipeborba@gmail.com>
2018-02-24 13:24:21 -0300

    bug fix on behaviour of tool

--- ProductionTools/CopyPasteTool/multiLayerSelect.py
+++ ProductionTools/CopyPasteTool/multiLayerSelect.py
@@ -260,6 +260,8 @@ class MultiLayerSelection(QgsMapTool):
         Selects a given feature on canvas.
         """
         idList = layer.selectedFeaturesIds()
+        self.iface.setActiveLayer(layer)
+        layer.startEditing()
         featId = feature.id()
         if featId not in idList:
             idList.append(featId)
