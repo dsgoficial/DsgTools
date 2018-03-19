22f1bc9704e99facef0db4ed766077a312760fd5
esperidiao <joao.p2709@gmail.com>
2018-03-19 12:03:43 -0300

    Flipping Line Tool initialized

---
+++ ProductionTools/FlippingLineTool/flippingLineTool.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ DsgTools
+                                 A QGIS plugin
+ Brazilian Army Cartographic Production Tools
+                             -------------------
+        begin                : 2018-03-19
+        git sha              : $Format:%H$
+        copyright            : (C) 2018 by João P. Esperidião - Cartographic Engineer @ Brazilian Army
+        email                : esperidiao.joao@eb.mil.br
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+from DsgTools.ProductionTools.CopyPasteTool.multiLayerSelect import MultiLayerSelection
+
+class FlippingLineTool(MultiLayerSelection):
+    def __init__(self):
+        pass
