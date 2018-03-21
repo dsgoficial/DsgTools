b778c508a06b7f00b1777ccafefd07be84654625
esperidiao <joao.p2709@gmail.com>
2018-03-21 11:38:44 -0300

    still missing reprojection and feature IDs changed message. Need work!

--- GeometricTools/DsgGeometryHandler.py
+++ GeometricTools/DsgGeometryHandler.py
@@ -20,7 +20,8 @@
  *                                                                         *
  ***************************************************************************/
 """
-from qgis.core import QgsMessageLog, QgsVectorLayer, QgsMapLayerRegistry, QgsGeometry, QgsField, QgsVectorDataProvider, QgsFeatureRequest, QgsExpression, QgsFeature, QgsSpatialIndex, QGis, QgsCoordinateTransform
+from qgis.core import QgsMessageLog, QgsVectorLayer, QgsMapLayerRegistry, QgsGeometry, QgsField, QgsVectorDataProvider, \
+                      QgsFeatureRequest, QgsExpression, QgsFeature, QgsSpatialIndex, QGis, QgsCoordinateTransform, QgsWKBTypes
 from PyQt4.Qt import QObject
 
 class DsgGeometryHandler(QObject):
@@ -61,21 +62,63 @@ class DsgGeometryHandler(QObject):
             coordinateTransformer = QgsCoordinateTransform(canvasCrs, destCrs)
             geom.transform(coordinateTransformer)
 
-    def flipFeature(self, layer, feature):
+    def flipFeature(self, layer, feature, geomType=None, refreshCanvas=False):
         """
-        Inverts the flow from a given feature. THE GIVEN FEATURE IS ALTERED.
+        Inverts the flow from a given feature. THE GIVEN FEATURE IS ALTERED. Standard behaviour is to not
+        refresh canvas map.
         :param layer: layer containing the target feature for flipping.
         :param feature: feature to be flipped.
-        :returns: flipped feature.
+        :param geomType: if layer geometry type is not given, it'll calculate it (0,1 or 2)
+        :param refreshCanvas: indicates whether the canvas should be refreshed after flipping feature.
+        :returns: flipped feature as of [layer, feature, geometry_type].
         """
+        if not geomType:
+            geomType = layer.geometryType()
+        # getting whether geometry is multipart or not
+        isMulti = QgsWKBTypes.isMultiType(int(layer.wkbType()))
         geom = feature.geometry()
-        nodes = geom.asPolyline()
-        flippedFeatureGeom = QgsGeometry.fromPolyline(nodes.reverse())
-        layer.changeGeometry(feature.id(), flippedFeatureGeom)
-        return flippedFeatureGeom
+        if geomType == 0:
+            if isMulti:
+                nodes = geom.asMultiPoint()
+                # inverting the point list by parts
+                for idx, part in enumerate(nodes):
+                    nodes[idx] = part[::-1]
+                # setting flipped geometry
+                flippedFeatureGeom = QgsGeometry.fromMultiPoint(nodes)                
+            else:
+                # inverting the point list
+                nodes = geom.asPoint()
+                nodes = nodes[::-1]
+                flippedFeatureGeom = QgsGeometry.fromPoint(nodes)                
+        elif geomType == 1:
+            if isMulti:
+                nodes = geom.asMultiPolyline()
+                for idx, part in enumerate(nodes):
+                    nodes[idx] = part[::-1]
+                flippedFeatureGeom = QgsGeometry.fromMultiPolyline(nodes)
+            else:
+                nodes = geom.asPolyline()
+                nodes = nodes[::-1]
+                flippedFeatureGeom = QgsGeometry.fromPolyline(nodes)         
+        elif geomType == 2:
+            if isMulti:
+                nodes = geom.asMultiPolygon()                
+                for idx, part in enumerate(nodes):
+                    nodes[idx] = part[::-1]
+                flippedFeatureGeom = QgsGeometry.fromMultiPolygon(nodes)                
+            else:
+                nodes = geom.asPolygon()
+                nodes = nodes[::-1]
+                flippedFeatureGeom = QgsGeometry.fromPolygon(nodes)
+        # setting feature geometry to the flipped one
+        feature.setGeometry(flippedFeatureGeom)
+        layer.updateFeature(feature)
+        if refreshCanvas:
+            self.iface.mapCanvas().refresh()
+        return [layer, feature, geomType]
     
     # MISSING REPROJECTION
-    def flipFeatureList(self, featureList, debugging=False):
+    def flipFeatureList(self, featureList, debugging=False, refreshCanvas=True):
         """
         Inverts the flow from all features of a given list. ALL GIVEN FEATURES ARE ALTERED.
         :param featureList: list of features to be flipped ([layer, feature[, geometry_type]).
@@ -90,11 +133,20 @@ class DsgGeometryHandler(QObject):
             if not isinstance(layer, QgsVectorLayer):
                 # ignore non-vector layers.
                 continue
+            # reprojecting feature to layer CRS
+            # self.reprojectFeature(feature.geometry, canvasCrs)
+            if len(item) == 3:
+                geomType = item[2]
+            else:
+                geomType = layer.geometryType()
             try:
-                reversedFeatureList.append(self.flipFeature(layer, feature))
+                revFeat = self.flipFeature(layer, feature, geomType)
+                reversedFeatureList.append(revFeat)
             except:
-                failedFeatureList.append(self.flipFeature(layer, feature))
-        if debugging and failedFeatureList:
+                failedFeatureList.append(item)
+        if refreshCanvas:
+            self.iface.mapCanvas().refresh()
+        if debugging:
             return reversedFeatureList, failedFeatureList
         else:
             return reversedFeatureList
