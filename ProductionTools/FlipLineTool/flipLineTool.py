7c69c57786d56a45d17cdb0379ac11fc028b5aa2
esperidiao <joao.p2709@gmail.com>
2018-03-21 14:05:07 -0300

    Flip Line Tool working

--- ProductionTools/FlipLineTool/flipLineTool.py
+++ ProductionTools/FlipLineTool/flipLineTool.py
@@ -21,42 +21,23 @@
  ***************************************************************************/
 """
 
-from qgis.gui import QgsMapTool, QgsRubberBand, QgsMessageBar
-from qgis.core import QGis, QgsPoint, QgsRectangle, QgsMapLayer, QgsFeatureRequest, QgsDataSourceURI, QgsVectorLayer, QgsMessageLog
+from qgis.gui import QgsMapTool, QgsMessageBar
+from qgis.core import QgsMapLayer, QgsVectorLayer, QgsMessageLog, QgsFeatureRequest
 from PyQt4 import QtCore, QtGui
 
-from DsgTools.ProductionTools.CopyPasteTool.multiLayerSelect import MultiLayerSelection
 from DsgTools.GeometricTools.DsgGeometryHandler import DsgGeometryHandler
 
-class FlipLine(MultiLayerSelection):
+class FlipLine(QgsMapTool):
     """
     Tool expected behaviour:
-    1- Right Click: selects the clicked feature (only lines)
-    2- Ctrl+ Right Click: 
+    When (valid) line features are selected, it flips them upon tool activation.
     """
     def __init__(self, canvas, iface):
-        super(FlipLine, self).__init__(canvas, iface)
+        self.iface = iface        
+        self.canvas = canvas
+        self.toolAction = None
+        QgsMapTool.__init__(self, self.canvas)
         self.DsgGeometryHandler = DsgGeometryHandler(iface)
-
-    def flipSelectedLines(self):
-        """
-        Method for instantiating tool.
-        """        
-        # get all selected features and remove all features that are not lines
-        selectedFeatures = self.getAllSelectedFeatures()
-        pop = 0
-        for idx, item in enumerate(selectedFeatures):
-            if item[2] != 1:
-                selectedFeatures.pop(idx-pop)
-                pop += 1
-        if not selectedFeatures:
-            self.iface.messageBar().pushMessage(self.tr('Error!'), self.tr('There are no lines selected!'), level=QgsMessageBar.CRITICAL, duration=3)
-            # QtGui.QMessageBox.critical(self, self.tr('Critical!'), self.tr("There are no lines selected!"))
-            # QgsMessageLog.logMessage(self.tr('Error flipping lines (did you select lines to be flipped?)'), "DSG Tools Plugin", QgsMessageLog.CRITICAL)
-            return
-        # call the method for flipping features from geometry module
-        flippedLines = self.DsgGeometryHandler.flipFeatureList(featureList=selectedFeatures)
-        print [line[1].id() for line in flippedLines]
 
     def activate(self):
         """
@@ -64,31 +45,19 @@ class FlipLine(MultiLayerSelection):
         """
         if self.toolAction:
             self.toolAction.setChecked(False)
-        # QgsMapTool.activate(self)
     
     def setAction(self, action):
+        """
+        Defines an action for tool.
+        action: QAction to be set.
+        """
         self.toolAction = action
-        # self.toolAction.setCheckable(True)
-
-    def checkSelectedLayers(self):
-        """
-        Reimplementation from parent class. Checks if there are any line type feature selected.
-        """
-        geom = None
-        for layer in self.iface.legendInterface().layers():
-            selection = layer.selectedFeatures()
-            if isinstance(layer, QgsVectorLayer):
-                if len(selection):
-                    if layer.geometryType() == 1:
-                        geom = layer.geometryType()
-                        break
-        return geom
 
     def canvasReleaseEvent(self, e):
         """
-        Reimplementation of parent class. Selects all line features inside the rectangle created by dragging
-        the mouse around canvas whilst holding Shift key.
-        After the rectangle is built, here features are selected.
+        Selects all line features inside the rectangle created by dragging the mouse around 
+        canvas whilst holding Shift key.
+        :param e: mouse event on canvas.
         """
         if QtGui.QApplication.keyboardModifiers() == QtCore.Qt.ShiftModifier:
             self.isEmittingPoint = False
@@ -104,9 +73,9 @@ class FlipLine(MultiLayerSelection):
                     layer.select(bbRect, True)
             self.rubberBand.hide()
 
-    def getAllSelectedFeatures(self):
+    def getAllSelectedLines(self):
         """
-        Reimplemented for parent class. Gets all selected lines on canvas.
+        Gets all selected lines on canvas.
         """
         selection = []
         for layer in self.iface.legendInterface().layers():
@@ -116,80 +85,133 @@ class FlipLine(MultiLayerSelection):
                 selection.append([layer, feat, layer.geometryType()])
         return selection
 
-    def flipLine(self, layer, line):
+    def flipSelectedLines(self):
         """
-        Flips the given line.
-        :param layer: layer containing the target feature
-        :param line: target feature to be flipped 
-        """
-        print "INVERTEU {} (id={})!".format(layer.name(), line.id())
-
-    def flipLineList(self, lineList):
-        """
-        Flips all lines in a given list.
-        :param lineList: a list os items as of [layer, line_feature[, geometry_type]]
-        """
-        for item in lineList:
-             self.flipLine(layer=item[0], line=item[1])
-            
-    def createContextMenu(self, e):
-        """
-        Reimplementation of parent method. Context Menu created to support feature selection.
-        :param e: mouse event caught from canvas
-        """ 
-        selected = (QtGui.QApplication.keyboardModifiers() == QtCore.Qt.ControlModifier)
-        # setting a list of features to iterate over
-        layerList = self.getPrimitiveDict(e, hasControlModifyer=selected)
-        # getting all features that are already selected on canvas
-        t = self.getAllSelectedFeatures()
-        # only line should be dealt with by this tool
-        if 1 in layerList.keys():
-            layers = layerList[1]
-        elif not layers:
+        Method for flipping all selected lines. Used for button callback.
+        """        
+        # get all selected features and remove all features that are not lines
+        selectedFeatures = self.getAllSelectedLines()
+        pop = 0
+        for idx, item in enumerate(selectedFeatures):
+            if item[2] != 1:
+                selectedFeatures.pop(idx-pop)
+                pop += 1
+        if not selectedFeatures:
+            logMsg = self.getLogMessage(None, None)
+            self.iface.messageBar().pushMessage(self.tr('Error'), logMsg, level=QgsMessageBar.CRITICAL, duration=3)
+            # QtGui.QMessageBox.critical(self, self.tr('Critical!'), logMsg)
+            QgsMessageLog.logMessage(logMsg, "DSG Tools Plugin", QgsMessageLog.CRITICAL)
             return
-        if layers:
-            menu = QtGui.QMenu()
-            rect = self.getCursorRect(e)
-            for layer in layers:
-                if not isinstance(layer, QgsVectorLayer):
-                    continue
-                # iterate over features inside the mouse bounding box 
-                bbRect = self.canvas.mapSettings().mapToLayerCoordinates(layer, rect)
-                for feature in layer.getFeatures(QgsFeatureRequest(bbRect)):
-                    geom = feature.geometry()
-                    if geom:
-                        if geom.intersects(rect):
-                            t.append([layer, feature, layer.geometryType()])
-            if len(t) > 1:
-                pop = 0 # number of features 
-                for i in range(0, len(t)):
-                    [layer, feature, geom] = t[i-pop] # geom to avoid dimension issues
-                    # layers from different dabases may have the same name
-                    # hence the need of db_name
-                    self.iface.setActiveLayer(layer) # a layer must be active in order to get db_name
-                    dsUri = self.iface.activeLayer().dataProvider().dataSourceUri()
-                    if '/' in dsUri:
-                        db_name = dsUri
-                    else:
-                        db_name = self.iface.activeLayer().dataProvider().dataSourceUri().split("'")[1]
-                    s = '{0}.{1} (feat_id = {2})'.format(db_name, layer.name(), feature.id())
-                    action = menu.addAction(s)
-                    # handling CTRL key and left/right click actions
-                    if e.button() == QtCore.Qt.LeftButton: 
-                            # line added to make sure the action is associated with current loop value,
-                            # lambda function is used with standard parameter set to current loops value.
-                            triggeredAction = lambda t=t[i] : self.setSelectionFeature(layer=t[0], feature=t[1], selectAll=selected)
-                            hoveredAction = lambda t=t[i] : self.createRubberBand(feature=t[1], layer=t[0], geom=t[2])
-                    self.addActionToMenu(action=action, onTriggeredAction=triggeredAction, onHoveredAction=hoveredAction)
-                # setting the action for the "All" options
-                if e.button() == QtCore.Qt.LeftButton:
-                    action = menu.addAction(self.tr('Select All Lines'))
-                    triggeredAction = lambda t=t: self.setSelectionListFeature(listLayerFeature=t)
-                # to trigger "Hover" signal on QMenu for the multiple options
-                hoveredAction = lambda t=t : self.createMultipleRubberBand(featureList=t)
-                self.addActionToMenu(action=action, onTriggeredAction=triggeredAction, onHoveredAction=hoveredAction)
-                menu.exec_(self.canvas.viewport().mapToGlobal(e.pos()))
-            elif t:
-                t = t[0]
-                if e.button() == QtCore.Qt.LeftButton:
-                    self.selectFeatures(e, hasControlModifyer = selected)
+        # call the method for flipping features from geometry module
+        flippedLines, failedLines = self.DsgGeometryHandler.flipFeatureList(featureList=selectedFeatures, debugging=True)
+        logMsg = self.getLogMessage(flippedLines, failedLines)
+        self.iface.messageBar().pushMessage(self.tr('Success'), logMsg, level=QgsMessageBar.INFO, duration=3)
+        QgsMessageLog.logMessage(logMsg, "DSG Tools Plugin", QgsMessageLog.INFO)
+ 
+    def getLogMessage(self, flippedLines, failedLines):
+        """
+        Method for mounting log message to be exposed to user.
+        :param flippedLines: list of lines that were selected and were successfully flipped.
+        :param failedLines: list of lines that were selected and failed to be flipped.
+        :param success: indicates whether the log is for a failed execution or 
+        """
+        nrFlipped = nrFailed = 0  
+        if not flippedLines and not failedLines:
+            return self.tr('There are no (valid) lines selected!')
+        if flippedLines:
+            nrFlipped = len(flippedLines)
+            logMsg = self.tr("Feature(s) flipped: ")
+            for item in flippedLines:
+                logMsg += "{} (id={}), ".format(item[0].name(), item[1].id())
+            logMsg = (logMsg + ")").replace(", )", "")
+        elif failedLines:
+            nrFailed = len(failedLines)
+            logMsg += self.tr("\nFeature(s) that failed to be flipped: ")
+            for item in failedLines:
+                logMsg += "{} (id={}), ".format(item[0].name(), item[1].id())
+            logMsg = (logMsg + ")").replace(", )", ".")
+        return logMsg + self.tr("\n{} lines flipped. {} failed to be flipped.").format(nrFlipped, nrFailed)
+
+    def startFlipLineTool(self):
+        if self.canvas.currentLayer() in self.iface.editableLayers():
+            self.flipSelectedLines()
+        else:
+            self.iface.messageBar().pushMessage(self.tr('Warning'), self.tr('Start editing in current layer!'), level=QgsMessageBar.INFO, duration=3)
+
+    # def flipLine(self, layer, line):
+    #     """
+    #     Flips the given line.
+    #     :param layer: layer containing the target feature
+    #     :param line: target feature to be flipped 
+    #     """
+    #     print "INVERTEU {} (id={})!".format(layer.name(), line.id())
+
+    # def flipLineList(self, lineList):
+    #     """
+    #     Flips all lines in a given list.
+    #     :param lineList: a list os items as of [layer, line_feature[, geometry_type]]
+    #     """
+    #     for item in lineList:
+    #          self.flipLine(layer=item[0], line=item[1])
+            
+    # def createContextMenu(self, e):
+    #     """
+    #     Reimplementation of parent method. Context Menu created to support feature selection.
+    #     :param e: mouse event caught from canvas
+    #     """ 
+    #     selected = (QtGui.QApplication.keyboardModifiers() == QtCore.Qt.ControlModifier)
+    #     # setting a list of features to iterate over
+    #     layerList = self.getPrimitiveDict(e, hasControlModifyer=selected)
+    #     # getting all features that are already selected on canvas
+    #     t = self.getAllSelectedFeatures()
+    #     # only line should be dealt with by this tool
+    #     if 1 in layerList.keys():
+    #         layers = layerList[1]
+    #     elif not layers:
+    #         return
+    #     if layers:
+    #         menu = QtGui.QMenu()
+    #         rect = self.getCursorRect(e)
+    #         for layer in layers:
+    #             if not isinstance(layer, QgsVectorLayer):
+    #                 continue
+    #             # iterate over features inside the mouse bounding box 
+    #             bbRect = self.canvas.mapSettings().mapToLayerCoordinates(layer, rect)
+    #             for feature in layer.getFeatures(QgsFeatureRequest(bbRect)):
+    #                 geom = feature.geometry()
+    #                 if geom:
+    #                     if geom.intersects(rect):
+    #                         t.append([layer, feature, layer.geometryType()])
+    #         if len(t) > 1:
+    #             pop = 0 # number of features 
+    #             for i in range(0, len(t)):
+    #                 [layer, feature, geom] = t[i-pop] # geom to avoid dimension issues
+    #                 # layers from different dabases may have the same name
+    #                 # hence the need of db_name
+    #                 self.iface.setActiveLayer(layer) # a layer must be active in order to get db_name
+    #                 dsUri = self.iface.activeLayer().dataProvider().dataSourceUri()
+    #                 if '/' in dsUri:
+    #                     db_name = dsUri
+    #                 else:
+    #                     db_name = self.iface.activeLayer().dataProvider().dataSourceUri().split("'")[1]
+    #                 s = '{0}.{1} (feat_id = {2})'.format(db_name, layer.name(), feature.id())
+    #                 action = menu.addAction(s)
+    #                 # handling CTRL key and left/right click actions
+    #                 if e.button() == QtCore.Qt.LeftButton: 
+    #                         # line added to make sure the action is associated with current loop value,
+    #                         # lambda function is used with standard parameter set to current loops value.
+    #                         triggeredAction = lambda t=t[i] : self.setSelectionFeature(layer=t[0], feature=t[1], selectAll=selected)
+    #                         hoveredAction = lambda t=t[i] : self.createRubberBand(feature=t[1], layer=t[0], geom=t[2])
+    #                 self.addActionToMenu(action=action, onTriggeredAction=triggeredAction, onHoveredAction=hoveredAction)
+    #             # setting the action for the "All" options
+    #             if e.button() == QtCore.Qt.LeftButton:
+    #                 action = menu.addAction(self.tr('Select All Lines'))
+    #                 triggeredAction = lambda t=t: self.setSelectionListFeature(listLayerFeature=t)
+    #             # to trigger "Hover" signal on QMenu for the multiple options
+    #             hoveredAction = lambda t=t : self.createMultipleRubberBand(featureList=t)
+    #             self.addActionToMenu(action=action, onTriggeredAction=triggeredAction, onHoveredAction=hoveredAction)
+    #             menu.exec_(self.canvas.viewport().mapToGlobal(e.pos()))
+    #         elif t:
+    #             t = t[0]
+    #             if e.button() == QtCore.Qt.LeftButton:
+    #                 self.selectFeatures(e, hasControlModifyer = selected)
