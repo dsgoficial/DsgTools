684a35cdac0b05fb8a6706cd5f6c06d9889f4b7f
phborba <philipeborba@gmail.com>
2018-04-13 11:46:25 -0300

    ui fix

--- gui/ProductionTools/Toolbars/InspectFeatures/inspectFeatures_ui.py
+++ gui/ProductionTools/Toolbars/InspectFeatures/inspectFeatures_ui.py
@@ -2,88 +2,75 @@
 
 # Form implementation generated from reading ui file 'inspectFeatures.ui'
 #
-# Created: Wed Jul 26 08:46:19 2017
-#      by: qgis.PyQt UI code generator 4.10.4
+# Created by: PyQt5 UI code generator 5.5.1
 #
 # WARNING! All changes made in this file will be lost!
 
-from builtins import object
-from qgis.PyQt import QtCore, QtWidgets, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtWidgets.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtWidgets.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtWidgets.QApplication.translate(context, text, disambig)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_Form(object):
     def setupUi(self, Form):
-        Form.setObjectName(_fromUtf8("Form"))
-        Form.resize(625, 24)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        Form.setObjectName("Form")
+        Form.resize(858, 36)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.MinimumExpanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(Form.sizePolicy().hasHeightForWidth())
         Form.setSizePolicy(sizePolicy)
-        Form.setMinimumSize(QtCore.QSize(0, 50))
+        Form.setMinimumSize(QtCore.QSize(0, 20))
         Form.setMaximumSize(QtCore.QSize(900, 50))
-        Form.setToolTip(_fromUtf8(""))
+        Form.setToolTip("")
         self.gridLayout = QtWidgets.QGridLayout(Form)
-        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setSpacing(0)
+        self.gridLayout.setObjectName("gridLayout")
         self.splitter = QtWidgets.QSplitter(Form)
         self.splitter.setOrientation(QtCore.Qt.Horizontal)
-        self.splitter.setObjectName(_fromUtf8("splitter"))
-        self.mMapLayerComboBox = gui.QgsMapLayerComboBox(self.splitter)
+        self.splitter.setObjectName("splitter")
+        self.mMapLayerComboBox = QgsMapLayerComboBox(self.splitter)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.mMapLayerComboBox.sizePolicy().hasHeightForWidth())
         self.mMapLayerComboBox.setSizePolicy(sizePolicy)
-        self.mMapLayerComboBox.setMaximumSize(QtCore.QSize(16777215, 24))
+        self.mMapLayerComboBox.setMinimumSize(QtCore.QSize(0, 20))
+        self.mMapLayerComboBox.setMaximumSize(QtCore.QSize(16777215, 32))
         self.mMapLayerComboBox.setFilters(core.QgsMapLayerProxyModel.HasGeometry|core.QgsMapLayerProxyModel.LineLayer|core.QgsMapLayerProxyModel.NoGeometry|core.QgsMapLayerProxyModel.PluginLayer|core.QgsMapLayerProxyModel.PointLayer|core.QgsMapLayerProxyModel.PolygonLayer|core.QgsMapLayerProxyModel.VectorLayer)
-        self.mMapLayerComboBox.setObjectName(_fromUtf8("mMapLayerComboBox"))
+        self.mMapLayerComboBox.setObjectName("mMapLayerComboBox")
         self.label_zoom = QtWidgets.QLabel(self.splitter)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.label_zoom.sizePolicy().hasHeightForWidth())
         self.label_zoom.setSizePolicy(sizePolicy)
-        self.label_zoom.setMinimumSize(QtCore.QSize(0, 0))
+        self.label_zoom.setMinimumSize(QtCore.QSize(0, 20))
         self.label_zoom.setMaximumSize(QtCore.QSize(16777215, 16777215))
         font = QtGui.QFont()
         font.setPointSize(9)
         self.label_zoom.setFont(font)
         self.label_zoom.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_zoom.setObjectName(_fromUtf8("label_zoom"))
-        self.mScaleWidget = gui.QgsScaleWidget(self.splitter)
+        self.label_zoom.setObjectName("label_zoom")
+        self.mScaleWidget = QgsScaleWidget(self.splitter)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.mScaleWidget.sizePolicy().hasHeightForWidth())
         self.mScaleWidget.setSizePolicy(sizePolicy)
-        self.mScaleWidget.setMinimumSize(QtCore.QSize(0, 24))
-        self.mScaleWidget.setMaximumSize(QtCore.QSize(16194919, 24))
+        self.mScaleWidget.setMinimumSize(QtCore.QSize(0, 20))
+        self.mScaleWidget.setMaximumSize(QtCore.QSize(16194919, 32))
         self.mScaleWidget.setShowCurrentScaleButton(True)
-        self.mScaleWidget.setObjectName(_fromUtf8("mScaleWidget"))
+        self.mScaleWidget.setObjectName("mScaleWidget")
         self.idSpinBox = QtWidgets.QSpinBox(self.splitter)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.idSpinBox.sizePolicy().hasHeightForWidth())
         self.idSpinBox.setSizePolicy(sizePolicy)
-        self.idSpinBox.setMinimumSize(QtCore.QSize(40, 24))
+        self.idSpinBox.setMinimumSize(QtCore.QSize(40, 20))
         self.idSpinBox.setMaximumSize(QtCore.QSize(16777215, 16777215))
         self.idSpinBox.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
-        self.idSpinBox.setSuffix(_fromUtf8(""))
-        self.idSpinBox.setObjectName(_fromUtf8("idSpinBox"))
+        self.idSpinBox.setSuffix("")
+        self.idSpinBox.setObjectName("idSpinBox")
         self.backInspectButton = QtWidgets.QPushButton(self.splitter)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
@@ -92,12 +79,12 @@ class Ui_Form(object):
         self.backInspectButton.setSizePolicy(sizePolicy)
         self.backInspectButton.setMinimumSize(QtCore.QSize(24, 24))
         self.backInspectButton.setMaximumSize(QtCore.QSize(24, 24))
-        self.backInspectButton.setText(_fromUtf8(""))
+        self.backInspectButton.setText("")
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/plugins/DsgTools/icons/backInspect.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap(":/plugins/DsgTools/icons/backInspect.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.backInspectButton.setIcon(icon)
         self.backInspectButton.setIconSize(QtCore.QSize(16, 16))
-        self.backInspectButton.setObjectName(_fromUtf8("backInspectButton"))
+        self.backInspectButton.setObjectName("backInspectButton")
         self.nextInspectButton = QtWidgets.QPushButton(self.splitter)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
@@ -106,20 +93,21 @@ class Ui_Form(object):
         self.nextInspectButton.setSizePolicy(sizePolicy)
         self.nextInspectButton.setMinimumSize(QtCore.QSize(24, 24))
         self.nextInspectButton.setMaximumSize(QtCore.QSize(24, 24))
-        self.nextInspectButton.setText(_fromUtf8(""))
+        self.nextInspectButton.setText("")
         icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap(_fromUtf8(":/plugins/DsgTools/icons/nextInspect.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon1.addPixmap(QtGui.QPixmap(":/plugins/DsgTools/icons/nextInspect.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.nextInspectButton.setIcon(icon1)
         self.nextInspectButton.setIconSize(QtCore.QSize(16, 16))
-        self.nextInspectButton.setObjectName(_fromUtf8("nextInspectButton"))
-        self.mFieldExpressionWidget = gui.QgsFieldExpressionWidget(self.splitter)
+        self.nextInspectButton.setObjectName("nextInspectButton")
+        self.mFieldExpressionWidget = QgsFieldExpressionWidget(self.splitter)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.mFieldExpressionWidget.sizePolicy().hasHeightForWidth())
         self.mFieldExpressionWidget.setSizePolicy(sizePolicy)
-        self.mFieldExpressionWidget.setObjectName(_fromUtf8("mFieldExpressionWidget"))
-        self.gridLayout.addWidget(self.splitter, 0, 1, -1, -1)
+        self.mFieldExpressionWidget.setMinimumSize(QtCore.QSize(0, 20))
+        self.mFieldExpressionWidget.setObjectName("mFieldExpressionWidget")
+        self.gridLayout.addWidget(self.splitter, 0, 2, 1, 1)
         self.inspectPushButton = QtWidgets.QPushButton(Form)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
@@ -128,25 +116,29 @@ class Ui_Form(object):
         self.inspectPushButton.setSizePolicy(sizePolicy)
         self.inspectPushButton.setMinimumSize(QtCore.QSize(24, 24))
         self.inspectPushButton.setMaximumSize(QtCore.QSize(24, 24))
-        self.inspectPushButton.setText(_fromUtf8(""))
+        self.inspectPushButton.setText("")
         icon2 = QtGui.QIcon()
-        icon2.addPixmap(QtGui.QPixmap(_fromUtf8(":/plugins/DsgTools/icons/inspectFeatures.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon2.addPixmap(QtGui.QPixmap(":/plugins/DsgTools/icons/inspectFeatures.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.inspectPushButton.setIcon(icon2)
         self.inspectPushButton.setIconSize(QtCore.QSize(16, 16))
         self.inspectPushButton.setCheckable(True)
-        self.inspectPushButton.setObjectName(_fromUtf8("inspectPushButton"))
-        self.gridLayout.addWidget(self.inspectPushButton, 0, 0, -1, -1)
+        self.inspectPushButton.setObjectName("inspectPushButton")
+        self.gridLayout.addWidget(self.inspectPushButton, 0, 0, 1, 1)
+        spacerItem = QtWidgets.QSpacerItem(5, 20, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem, 0, 1, 1, 1)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
-        Form.setWindowTitle(_translate("Form", "Form", None))
-        self.label_zoom.setText(_translate("Form", "Scale", None))
-        self.idSpinBox.setPrefix(_translate("Form", "ID: ", None))
-        self.backInspectButton.setToolTip(_translate("Form", "<html><head/><body><p>Back inspect</p><p><br/></p></body></html>", None))
-        self.nextInspectButton.setToolTip(_translate("Form", "<html><head/><body><p>Next inspect</p></body></html>", None))
-        self.inspectPushButton.setToolTip(_translate("Form", "Inspect Features Tool", None))
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label_zoom.setText(_translate("Form", "Scale"))
+        self.idSpinBox.setPrefix(_translate("Form", "ID: "))
+        self.backInspectButton.setToolTip(_translate("Form", "<html><head/><body><p>Back inspect</p><p><br/></p></body></html>"))
+        self.nextInspectButton.setToolTip(_translate("Form", "<html><head/><body><p>Next inspect</p></body></html>"))
+        self.inspectPushButton.setToolTip(_translate("Form", "Inspect Features Tool"))
 
-from qgis import gui, core  
+from qgis.gui import QgsFieldExpressionWidget, QgsMapLayerComboBox, QgsScaleWidget
+from qgis import core
 import resources_rc
