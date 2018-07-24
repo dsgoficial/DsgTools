24b4896f9c83e23a6e45c3475e0c9852a8ef3325
phborba <philipeborba@gmail.com>
2018-07-24 18:55:52 -0300

    parameter fetcher changes

--- gui/ProductionTools/Toolboxes/ValidationToolbox/parameterFetcher.py
+++ gui/ProductionTools/Toolboxes/ValidationToolbox/parameterFetcher.py
@@ -22,8 +22,11 @@
 """
 from __future__ import print_function
 import os
+from collections import deque, OrderedDict
 from qgis.core import QgsMessageLog, Qgis, QgsTask, QgsApplication
 from DsgTools.gui.ProductionTools.Toolboxes.ValidationToolbox.processParametersDialog import ProcessParametersDialog
+from DsgTools.gui.CustomWidgets.OrderedPropertyWidgets.orderedRecursiveSnapWidget import OrderedRecursiveSnapWidget
+from DsgTools.core.ValidationTools.ValidationProcesses.hierarchicalSnapLayerOnLayerProcess import HierarchicalSnapParameters
 
 from qgis.PyQt.QtCore import Qt
 from qgis.PyQt import QtGui
@@ -37,6 +40,80 @@ class ParameterFetcher(QObject):
         Constructor
         """
         super(ParameterFetcher, self).__init__()
+        self.processAliasDict = {
+            'Rules Setting' : {
+                'alias': self.tr('Rules Setting'), 
+                'type': deque
+                },
+            'Snap' : {
+                'alias': self.tr('Snap'), 
+                'type': float
+                },
+            'MinArea' : {
+                'alias': self.tr('Minimum area'), 
+                'type': float
+                },
+            'Layers' : {
+                'alias': self.tr('Layers'), 
+                'type': list
+                },
+            'Only Selected' : {
+                'alias': self.tr('Only Selected'), 
+                'type': bool
+                },
+            'MaxDissolveArea' : {
+                'alias': self.tr('Maximum dissolved area'), 
+                'type': float
+                },
+            'Ordered Layers' : {
+                'alias': self.tr('Ordered Layers'), 
+                'type': HierarchicalSnapParameters
+                },
+            'Ignore search radius on inner layer search' : {
+                'alias': self.tr('Ignore search radius on inner layer search'), 
+                'type': bool
+                }
+            'Layer and Filter Layers' : {
+                'alias': self.tr('Ordered Layers'), 
+                'type': OrderedDict
+                },
+            'Identification Type' : {
+                'alias': self.tr('Identification Type'), 
+                'type': deque
+            },
+            'Reference and Layers' : {
+                'alias': self.tr('Reference and Layers'),
+                'type' : OrderedDict
+            },
+            'Angle' : {
+                'alias': self.tr('Reference and Layers'),
+                'type' : float
+            },
+            'Only First Order Lines' : {
+                'alias': self.tr('Only First Order Lines'),
+                'type' : float
+            }
+            'Tolerance' : {
+                'alias': self.tr('Tolerance'),
+                'type' : float
+            },
+            'AttributeBlackList' : {
+                'alias': self.tr('Attribute Black List (comma separated)'),
+                'type' : str
+            },
+            'Overlayer and Layers' : {
+                'alias': self.tr('Overlay layer and layers to be overlayed'),
+                'type' : OrderedDict
+            },
+            'Overlay Type' : {
+                'alias' : self.tr('Overlay type'),
+                'type' : deque
+            },
+            'Coordinate Precision' : {
+                'alias' : self.tr('Coordinate Precision'),
+                'type' : float
+            }
+        }
     
     def fetch(self, processChain, parameterDict, restoreOverride = True):
         """
@@ -50,3 +127,4 @@ class ParameterFetcher(QObject):
         # get parameters
         params = dlg.values
         return params
+    
