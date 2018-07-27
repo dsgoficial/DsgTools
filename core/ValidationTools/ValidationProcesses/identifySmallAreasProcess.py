c53c7fe2d0b9432d172b302e132ea124ebfd31b9
phborba <philipeborba@gmail.com>
2018-07-27 11:44:48 -0300

    more changes

--- core/ValidationTools/ValidationProcesses/identifySmallAreasProcess.py
+++ core/ValidationTools/ValidationProcesses/identifySmallAreasProcess.py
@@ -26,6 +26,160 @@ from DsgTools.core.ValidationTools.ValidationProcesses.validationProcess import 
 from DsgTools.gui.CustomWidgets.BasicInterfaceWidgets.progressWidget import ProgressWidget
 import binascii
 
+from PyQt5.QtCore import QCoreApplication
+from qgis.core import (QgsProcessing,
+                       QgsFeatureSink,
+                       QgsProcessingAlgorithm,
+                       QgsProcessingParameterFeatureSource,
+                       QgsProcessingParameterFeatureSink,
+                       QgsFeature,
+                       QgsDataSourceUri,
+                       QgsProcessingOutputVectorLayer,
+                       QgsProcessingParameterVectorLayer,
+                       QgsWkbTypes,
+                       QgsProcessingParameterBoolean)
+
+class IdentifySmallAreaAlgorithm(QgsProcessingAlgorithm):
+    OUTPUT = 'FLAGS'
+    INPUT = 'INPUT'
+    SELECTED = 'SELECTED'
+
+    def initAlgorithm(self, config):
+        """
+        Parameter setting.
+        """
+        self.addParameter(
+            QgsProcessingParameterVectorLayer(
+                self.INPUT,
+                self.tr('Input layer'),
+                [QgsProcessing.TypeVectorPolygon ]
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterBoolean(
+                self.SELECTED,
+                self.tr('Process only selected features')
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterFeatureSink(
+                self.OUTPUT,
+                self.tr('Flag layer')
+            )
+        )
+
+    def processAlgorithm(self, parameters, context, feedback):
+        """
+        Here is where the processing itself takes place.
+        """
+
+        # Retrieve the feature source and sink. The 'dest_id' variable is used
+        # to uniquely identify the feature sink, and must be included in the
+        # dictionary returned by the processAlgorithm function.
+        source = self.parameterAsSource(parameters, self.INPUT, context)
+        # (sink, dest_id) = self.parameterAsSink(parameters, self.OUTPUT,
+        #         context, source.fields(), source.wkbType(), source.sourceCrs())
+
+        onlySelected = self.parameterAsBool(parameters, self.SELECTED, context)
+
+        inputLyr = self.parameterAsVectorLayer(parameters, self.INPUT, context)
+
+        target.startEditing()
+        target.beginEditCommand('Updating layer')
+        provider = target.dataProvider()
+        uri = QgsDataSourceUri(provider.dataSourceUri())
+        keyColumn = uri.keyColumn()
+        destType = target.geometryType()
+        destIsMulti = QgsWkbTypes.isMultiType(target.wkbType())
+
+        # Compute the number of steps to display within the progress bar and
+        # get features from source
+        features, total = self.getIteratorAndFeatureCount(inputLyr, onlySelected=onlySelected)           
+
+        for current, feature in enumerate(features):
+            # Stop the algorithm if cancel button has been clicked
+            if feedback.isCanceled():
+                break
+            if feat.geometry().area() < tol:
+                
+            geom = feature.geometry()
+            if not geom:
+                target.deleteFeature(feature.id())
+                # Update the progress bar
+                feedback.setProgress(int(current * total))
+                continue
+            if geom.get().partCount() > 1:
+                parts = geom.asGeometryCollection()
+                for part in parts:
+                    if destIsMulti:
+                        part.convertToMultiType()
+                addList = []
+                for i in range(1,len(parts)):
+                    if parts[i]:
+                        newFeat = QgsFeature(feature)
+                        newFeat.setGeometry(parts[i])
+                        idx = newFeat.fieldNameIndex(keyColumn)
+                        newFeat.setAttribute(idx,provider.defaultValue(idx))
+                        addList.append(newFeat)
+                feature.setGeometry(parts[0])
+                target.updateFeature(feature)
+                target.addFeatures(addList, QgsFeatureSink.FastInsert)             
+
+            # Update the progress bar
+            feedback.setProgress(int(current * total))
+
+        target.endEditCommand()
+
+        # Return the results of the algorithm. In this case our only result is
+        # the feature sink which contains the processed features, but some
+        # algorithms may return multiple feature sinks, calculated numeric
+        # statistics, etc. These should all be included in the returned
+        # dictionary, with keys matching the feature corresponding parameter
+        # or output names.
+        return {self.OUTPUT: target}
+
+    def name(self):
+        """
+        Returns the algorithm name, used for identifying the algorithm. This
+        string should be fixed for the algorithm, and must not be localised.
+        The name should be unique within each provider. Names should contain
+        lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'deaggregategeometries'
+
+    def displayName(self):
+        """
+        Returns the translated algorithm name, which should be used for any
+        user-visible display of the algorithm name.
+        """
+        return self.tr('Deaggregate Geometries')
+
+    def group(self):
+        """
+        Returns the name of the group this algorithm belongs to. This string
+        should be localised.
+        """
+        return self.tr('Validation Tools')
+
+    def groupId(self):
+        """
+        Returns the unique ID of the group this algorithm belongs to. This
+        string should be fixed for the algorithm, and must not be localised.
+        The group id should be unique within each provider. Group id should
+        contain lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'DSGTools: Validation Tools'
+
+    def tr(self, string):
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return DeaggregatorAlgorithm()
+
 class IdentifySmallAreasProcess(ValidationProcess):
     def __init__(self, postgisDb, iface, instantiating = False, withElements = True):
         """
