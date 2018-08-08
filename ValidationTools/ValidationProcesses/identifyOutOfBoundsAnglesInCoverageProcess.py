1115ef58475e90dfdd7bcfb4739c8b22122f9987
phborba <philipeborba@gmail.com>
2018-08-08 16:31:16 -0300

    class metadada changes

--- ValidationTools/ValidationProcesses/identifyOutOfBoundsAnglesInCoverageProcess.py
+++ ValidationTools/ValidationProcesses/identifyOutOfBoundsAnglesInCoverageProcess.py
@@ -5,10 +5,10 @@
                                  A QGIS plugin
  Brazilian Army Cartographic Production Tools
                               -------------------
-        begin                : 2016-02-18
+        begin                : 2018-06-08
         git sha              : $Format:%H$
-        copyright            : (C) 2016 by Luiz Andrade - Cartographic Engineer @ Brazilian Army
-        email                : luiz.claudio@dsg.eb.mil.br
+        copyright            : (C) 2018 by Philipe Borba - Cartographic Engineer @ Brazilian Army
+        email                : borba.philipe@eb.mil.br
  ***************************************************************************/
 
 /***************************************************************************
