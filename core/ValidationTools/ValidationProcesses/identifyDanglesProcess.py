bb535b64407171270c82db6b877856c0b660a381
phborba <philipeborba@gmail.com>
2018-08-12 02:20:13 -0300

    first prototype before tests

--- core/ValidationTools/ValidationProcesses/identifyDanglesProcess.py
+++ core/ValidationTools/ValidationProcesses/identifyDanglesProcess.py
@@ -35,6 +35,7 @@ from DsgTools.core.ValidationTools.ValidationProcesses.validationProcess import 
 from collections import deque, OrderedDict
 
 from DsgTools.core.ValidationTools.ValidationProcesses.validationProcess import ValidationAlgorithm, ValidationProcess
+from DsgTools.core.GeometricTools.layerHandler import LayerHandler
 
 from PyQt5.QtCore import QCoreApplication
 from qgis.core import (QgsProcessing,
@@ -104,13 +105,11 @@ class IdentifyDanglesAlgorithm(ValidationAlgorithm):
             )
         )
         self.addParameter(
-            QgsProcessingParameterEnum(
-                self.IGNOREINNER,
+            QgsProcessingParameterBoolean(
+                self.TYPE,
                 self.tr('Identification Type'),
-                options=[
-                    self.tr('Consider dangle on unsegmented lines'),
-                    self.tr('Ignore dangle on unsegmented lines')
-                ]
+                self.tr('Ignore dangle on unsegmented lines'),
+                defaultValue = False
             )
         )
         self.addParameter(
@@ -134,37 +133,184 @@ class IdentifyDanglesAlgorithm(ValidationAlgorithm):
 
         inputLyr = self.parameterAsVectorLayer(parameters, self.INPUT, context)
         onlySelected = self.parameterAsBool(parameters, self.SELECTED, context)
+        searchRadius = self.parameterAsDouble(parameters, self.TOLERANCE, context)
         lineFilterLyrList = self.parameterAsLayerList(parameters, self.LINEFILTERLAYERS, context)
         polygonFilterLyrList = self.parameterAsLayerList(parameters, self.POLYGONFILTERLAYERS, context)
-        if inputLyrList == []:
-            raise QgsProcessingException(self.invalidSourceError(parameters, self.LINEFILTERLAYERS))
+        ignoreNotSplit = self.parameterAsBool(parameters, self.TYPE, context)
+        ignoreInner = self.parameterAsBool(parameters, self.IGNOREINNER, context)
         self.prepareFlagSink(parameters, inputLyr, inputLyr.wkbType(), context)
+
         # Compute the number of steps to display within the progress bar and
         # get features from source
-        featureList, total = self.getIteratorAndFeatureCount(inputLyr)           
-        geomDict = dict()
+        featureList, total = self.getIteratorAndFeatureCount(inputLyr)
+        endVerticesDict = self.buildInitialAndEndPointDict(featureList, feedback, progressDelta=25)
+        #search for dangles candidates
+        pointList = self.searchDanglesOnPointDict(endVerticesDict, feedback, progressDelta=25)
+        #build filter layer
+        filterLayer = self.buildFilterLayer(lineLyrList, polygonLyrList, context, feedback, onlySelected=onlySelected)
+        #filter pointList with filterLayer
+        delta = 20 if not ignoreInner else 40
+        filteredPointList = self.filterPointListWithFilterLayer(pointList, filterLayer, searchRadius, feedback, progressDelta = delta)
+        #filter with own layer
+        if not ignoreInner: #True when looking for dangles on contour lines
+            filteredPointList = self.filterPointListWithFilterLayer(filteredPointList, reflyr, searchRadius, isRefLyr = True, ignoreNotSplit = ignoreNotSplit, progressDelta=20)
+        #build flag list with filtered points
+        if filteredPointList:
+            currentValue = feedback.progress()
+            currentTotal = 10/len(filteredPointList)
+            for current, point in enumerate(filteredPointList):
+                if feedback.isCanceled():
+                    break
+                self.flagFeature(QgsGeometry.fromPoint(point), self.tr('Dangle on {0}').format(inputLyr.name()))
+                feedback.setProgress(currentValue + int(current*currentTotal))      
+        feedback.setProgress(100)
+        return {self.FLAGS: self.flagSink}
+
+    def buildInitialAndEndPointDict(self, featureList, feedback, progressDelta = 100):
+        """
+        Calculates initial point and end point from each line from lyr.
+        """
+        # start and end points dict
+        currentProgress = feedback.progress()
+        endVerticesDict = dict()
+        localTotal = progressDelta/len(featureList)
+        # iterating over features to store start and end points
         for current, feat in enumerate(featureList):
-            # Stop the algorithm if cancel button has been clicked
             if feedback.isCanceled():
                 break
             geom = feat.geometry()
-            if isMulti and not geom.isMultipart():
-                geom.convertToMultiType()
-            geomKey = geom.asWkb()
-            if geomKey not in geomDict:
-                geomDict[geomKey] = []
-            geomDict[geomKey].append(feat)
-            # # Update the progress bar
-            feedback.setProgress(int(current * total))
-        for k, v in geomDict.items():
+            lineList = geom.asMultiPolyline() if geom.isMultipart() else [geom.asPolyline()]
+            for line in lineList:
+                self.addFeatToDict(endVerticesDict, line, feat.id())
+            feedback.setProgress(currentProgress + int(localTotal*current))
+        return endVerticesDict
+
+    def addFeatToDict(self, endVerticesDict, line, featid):
+        self.addPointToDict(line[0], endVerticesDict, featid)
+        self.addPointToDict(line[len(line) - 1], endVerticesDict, featid)
+    
+    def addPointToDict(self, point, pointDict, featid):
+        if point not in pointDict:
+            pointDict[point] = []
+        pointDict[point].append(featid)
+    
+    def searchDanglesOnPointDict(self, endVerticesDict, feedback, progressDelta = 100):
+        """
+        Counts the number of points on each endVerticesDict's key and returns a list of QgsPoint built from key candidate.
+        """
+        pointList = []
+        currentProgress = feedback.progress()
+        localTotal = progressDelta/len(featureList)
+        # actual search for dangles
+        for current, point in enumerate(endVerticesDict):
             if feedback.isCanceled():
                 break
-            if len(v) > 1:
-                idStrList = ','.join( map(str, [i.id() for i in v] ) )
-                flagText = self.tr('Features from layer {0} with ids=({1}) have duplicated geometries.').format(inputLyr.name(), idStrList)
-                self.flagFeature(v[0].geometry(), flagText)      
+            # this means we only have one occurrence of point, therefore it is a dangle
+            if len(endVerticesDict[point]) <= 1:
+                pointList.append(point)
+            feedback.setProgress(currentProgress + int(localTotal*current))
+        return pointList
 
-        return {self.FLAGS: self.flagSink}
+    def buildFilterLayer(self, lineLyrList, polygonLyrList, context, feedback, onlySelected = False):
+        """
+        Buils one layer of filter lines.
+        Build unified layer is not used because we do not care for attributes here, only geometry.
+        refLyr elements are also added.
+        """
+        layerHandler = LayerHandler()
+        lineLyrs = lineLyrList
+        for polygonLyr in polygonLyrList:
+            if feedback.isCanceled():
+                break
+            lineLyrs += self.makeBoundaries(polygonLyr, context, feedback)
+        unifiedLinesLyr = layerHandler.createAndPopulateUnifiedVectorLayer(lineLyrs, QgsWkbTypes.MultiLinestring, onlySelected = onlySelected)
+        filterLyr = self.cleanLayer(unifiedLinesLyr, [0,6], context)
+        return filterLyr
+    
+    def makeBoundaries(self, lyr, context, feedback):
+        parameters = {
+            'INPUT' : lyr,
+            'OUTPUT' : 'memory:'
+        }
+        output = processing.run("native:boundary", parameters, context = context, feedback = feedback)
+        return output['OUTPUT']
+
+    def cleanLayer(self, inputLyr, toolList, context, typeList=[0,1,2,3,4,5,6]): 
+        #TODO write one class that runs all processing stuff (model that tomorrow)
+        output = QgsProcessingUtils.generateTempFilename('output.shp')
+        error = QgsProcessingUtils.generateTempFilename('error.shp')
+        parameters = {
+            'input':inputLyr,
+            'type':typeList,
+            'tool':toolList,
+            'threshold':'-1', 
+            '-b':False, 
+            '-c':True, 
+            'output' : output, 
+            'error': error, 
+            'GRASS_REGION_PARAMETER':None,
+            'GRASS_SNAP_TOLERANCE_PARAMETER': -1,
+            'GRASS_MIN_AREA_PARAMETER': 0.0001,
+            'GRASS_OUTPUT_TYPE_PARAMETER': 0,
+            'GRASS_VECTOR_DSCO':'',
+            'GRASS_VECTOR_LCO':''
+            }
+        x = processing.run('grass7:v.clean', parameters, context = context)
+        lyr = QgsProcessingUtils.mapLayerFromString(x['output'], context)
+        return lyr
+
+    def filterPointListWithFilterLayer(self, pointList, filterLayer, searchRadius, feedback, progressDelta = 100, isRefLyr = False, ignoreNotSplit = False):
+        """
+        Builds buffer areas from each point and evaluates the intersecting lines. If there are more than two intersections, it is a dangle.
+        """
+        currentProgress = feedback.progress()
+        localTotal = progressDelta/len(pointList)
+        spatialIdx, allFeatureDict = self.buildSpatialIndexAndIdDict(filterLayer)
+        notDangleList = []
+        for current, point in enumerate(pointList):
+            if feedback.isCanceled():
+                break
+            candidateCount = 0
+            qgisPoint = QgsGeometry.fromPoint(point)
+            #search radius to narrow down candidates
+            buffer = qgisPoint.buffer(searchRadius, -1)
+            bufferBB = buffer.boundingBox()
+            #gets candidates from spatial index
+            candidateIds = spatialIdx.intersects(bufferBB)
+            #if there is only one feat in candidateIds, that means that it is not a dangle
+            bufferCount = len([id for id in candidateIds if buffer.intersects(allFeatureDict[id].geometry())])
+            for id in candidateIds:
+                if not isRefLyr:
+                    if buffer.intersects(allFeatureDict[id].geometry()) and \
+                    qgisPoint.distance(allFeatureDict[id].geometry()) < 10**-9: #float problem, tried with intersects and touches and did not get results
+                        notDangleIndexList.append(point)
+                        break
+                else:
+                    if ignoreNotSplit:
+                        if buffer.intersects(allFeatureDict[id].geometry()) and \
+                        (qgisPoint.distance(allFeatureDict[id].geometry()) < 10**-9 or \
+                        qgisPoint.intersects(allFeatureDict[id].geometry())): #float problem, tried with intersects and touches and did not get results
+                            candidateCount += 1
+                    else:
+                        if buffer.intersects(allFeatureDict[id].geometry()) and \
+                        (qgisPoint.touches(allFeatureDict[id].geometry())): #float problem, tried with intersects and touches and did not get results
+                            candidateCount += 1
+                    if candidateCount == bufferCount:
+                        notDangleIndexList.append(point)
+            feedback.setProgress(currentProgress + localTotal*current)
+        filteredDangleList = [point for point in pointList if point not in notDangleList]
+        return filteredDangleList
+    
+    def buildSpatialIndexAndIdDict(self, inputLyr):
+        """
+        creates a spatial index for the centroid layer
+        """
+        spatialIdx = QgsSpatialIndex()
+        idDict = {}
+        for feat in inputLyr.getFeatures():
+            spatialIdx.insertFeature(feat)
+            idDict[feat.id()] = feat
+        return spatialIdx, idDict
 
     def name(self):
         """
