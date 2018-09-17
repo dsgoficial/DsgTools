c8d7783088c20c002236769304e6ec09cd5bcccb
phborba <philipeborba@gmail.com>
2018-09-17 16:57:43 -0300

    partial commit

---
+++ core/DSGToolsProcessingAlgs/Algs/ValidationAlgs/overlayElementsWithAreasAlgorithm.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ DsgTools
+                                 A QGIS plugin
+ Brazilian Army Cartographic Production Tools
+                              -------------------
+        begin                : 2018-09-17
+        git sha              : $Format:%H$
+        copyright            : (C) 2018 by Philipe Borba - Cartographic Engineer @ Brazilian Army
+        email                : borba.philipe@eb.mil.br
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+from DsgTools.core.GeometricTools.layerHandler import LayerHandler
+from .validationAlgorithm import ValidationAlgorithm
+from ...algRunner import AlgRunner
+import processing
+from PyQt5.QtCore import QCoreApplication
+from qgis.core import (QgsProcessing,
+                       QgsFeatureSink,
+                       QgsProcessingAlgorithm,
+                       QgsProcessingParameterFeatureSource,
+                       QgsProcessingParameterFeatureSink,
+                       QgsFeature,
+                       QgsDataSourceUri,
+                       QgsProcessingOutputVectorLayer,
+                       QgsProcessingParameterVectorLayer,
+                       QgsWkbTypes,
+                       QgsProcessingParameterBoolean,
+                       QgsProcessingParameterEnum,
+                       QgsProcessingParameterNumber,
+                       QgsProcessingParameterMultipleLayers,
+                       QgsProcessingUtils,
+                       QgsSpatialIndex,
+                       QgsGeometry,
+                       QgsProject,
+                       QgsProcessingMultiStepFeedback,
+                       QgsProcessingParameterDistance)
+
+class OverlayElementsWithAreasAlgorithm(ValidationAlgorithm):
+    INPUT = 'INPUT'
+    SELECTED = 'SELECTED'
+    TOLERANCE = 'TOLERANCE'
+    MINAREA = 'MINAREA'
+
+    def initAlgorithm(self, config):
+        """
+        Parameter setting.
+        """
+        self.addParameter(
+            QgsProcessingParameterVectorLayer(
+                self.INPUT,
+                self.tr('Input Layer'),
+                [QgsProcessing.TypeVectorAnyGeometry]
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterBoolean(
+                self.SELECTED,
+                self.tr('Process only selected features')
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterDistance(
+                self.TOLERANCE, 
+                self.tr('Snap radius'), 
+                parentParameterName=self.INPUT,                                         
+                minValue=0, 
+                defaultValue=1.0
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterNumber(
+                self.MINAREA,
+                self.tr('Minimum area'),
+                minValue=0,
+                defaultValue=0.0001,
+                type=QgsProcessingParameterNumber.Double
+            )
+        )
+
+    def processAlgorithm(self, parameters, context, feedback):
+        """
+        Here is where the processing itself takes place.
+        """
+        layerHandler = LayerHandler()
+        algRunner = AlgRunner()
+
+        inputLyr = self.parameterAsVectorLayer(parameters, self.INPUT, context)
+        if inputLyr is None:
+            raise QgsProcessingException(self.invalidSourceError(parameters, self.INPUT))
+        onlySelected = self.parameterAsBool(parameters, self.SELECTED, context)
+        snap = self.parameterAsDouble(parameters, self.TOLERANCE, context)
+        minArea = self.parameterAsDouble(parameters, self.MINAREA, context)
+        self.prepareFlagSink(parameters, inputLyr, inputLyr.wkbType(), context)
+
+        multiStepFeedback = QgsProcessingMultiStepFeedback(3, feedback)
+        multiStepFeedback.setCurrentStep(0)
+        multiStepFeedback.pushInfo(self.tr('Populating temp layer...'))
+        auxLyr = layerHandler.createAndPopulateUnifiedVectorLayer([inputLyr], geomType=inputLyr.wkbType(), onlySelected = onlySelected, feedback=multiStepFeedback)
+        multiStepFeedback.setCurrentStep(1)
+        multiStepFeedback.pushInfo(self.tr('Running overlay...'))
+        cleanedLyr, error = algRunner.runOverlay(auxLyr, \
+                                                    [algRunner.RMSA, algRunner.Break, algRunner.RmDupl, algRunner.RmDangle], \
+                                                    context, \
+                                                    returnError=True, \
+                                                    snap=snap, \
+                                                    minArea=minArea,
+                                                    feedback=multiStepFeedback)
+        multiStepFeedback.setCurrentStep(2)
+        multiStepFeedback.pushInfo(self.tr('Updating original layer...'))
+        layerHandler.updateOriginalLayersFromUnifiedLayer([inputLyr], cleanedLyr, feedback=multiStepFeedback, onlySelected=onlySelected)
+        self.flagIssues(cleanedLyr, error, feedback)
+
+        return {self.INPUT : inputLyr, self.FLAGS : self.flag_id}
+
+    def name(self):
+        """
+        Returns the algorithm name, used for identifying the algorithm. This
+        string should be fixed for the algorithm, and must not be localised.
+        The name should be unique within each provider. Names should contain
+        lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'overlayelementswithareas'
+
+    def displayName(self):
+        """
+        Returns the translated algorithm name, which should be used for any
+        user-visible display of the algorithm name.
+        """
+        return self.tr('Overlay Elements With Areas')
+
+    def group(self):
+        """
+        Returns the name of the group this algorithm belongs to. This string
+        should be localised.
+        """
+        return self.tr('Validation Tools (Manipulation Processes)')
+
+    def groupId(self):
+        """
+        Returns the unique ID of the group this algorithm belongs to. This
+        string should be fixed for the algorithm, and must not be localised.
+        The group id should be unique within each provider. Group id should
+        contain lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'DSGTools: Validation Tools (Manipulation Processes)'
+
+    def tr(self, string):
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return OverlayElementsWithAreasAlgorithm()
