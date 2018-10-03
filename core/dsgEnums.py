20fe8c826d9a434b8835fbf409166c7dd2b17550
phborba <philipeborba@gmail.com>
2018-10-03 17:00:12 -0300

    not done yet

--- core/dsgEnums.py
+++ core/dsgEnums.py
@@ -31,3 +31,5 @@ class DsgEnums(object):
     PermissionProperty, FieldToolboxProperty, EarthCoverageProperty, AttributeRuleProperty, SpatialRuleProperty, ValidationWorkflowProperty = list(range(6))
     # datasource selection widgets enum
     NoDriver, PostGIS, NewPostGIS, SpatiaLite, NewSpatiaLite, Shapefile, NewShapefile, Geopackage, NewGeopackage = list(range(9))
+    # node types enum
+    Flag, Sink, WaterwayBegin, UpHillNode, DownHillNode, Confluence, Ramification, AttributeChange, NodeNextToWaterBody, AttributeChangeFlag, NodeOverload, DisconnectedLine = list(range(12))
