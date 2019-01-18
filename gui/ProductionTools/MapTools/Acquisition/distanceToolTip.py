64b426766f11ba530915fe39b1d99d3bce7990d2
esperidiao <joao.p2709@gmail.com>
2019-01-18 10:29:01 -0200

    #321 migration started. module distanceToolTip updated.

--- gui/ProductionTools/MapTools/Acquisition/distanceToolTip.py
+++ gui/ProductionTools/MapTools/Acquisition/distanceToolTip.py
@@ -30,12 +30,12 @@ from DsgTools.gui.ProductionTools.MapTools.Acquisition.toolTip import ToolTip
 
 class DistanceToolTip(ToolTip):
 	def __init__(self, iface):
-		super(DistanceToolTip, self).__init__(iface)
+		super(DistanceToolTip, self).__init__(iface, minSegmentDistance)
 		self.iface = iface
 		self.canvas = iface.mapCanvas()      
 		self.last_distance = 0  
-		self.showing = False	
-			
+		self.showing = False
+		self.minSegmentDistance = minSegmentDistance
 
 	def calculateDistance(self, p1, p2):
 		distance = QgsDistanceArea()
@@ -45,19 +45,15 @@ class DistanceToolTip(ToolTip):
 		m = distance.measureLine(p1, p2) 
 		return m
 
-	def deactivate(self):
-		super(DistanceToolTip, self).deactivate()
-		self.showing = False
-
 	def canvasMoveEvent(self, last_p, current_p):
 		m =  int(self.calculateDistance(last_p, current_p))
 		if self.showing:
 			if m != self.last_distance:
 				color = 'red'
-				if m >= 4:
+				if m >= self.minSegmentDistance:
 					color = 'green'				
 				txt = "<p style='color:{color}'><b>{distance}</b></p>".format(color=color, distance=str(m))
-				super(DistanceToolTip, self).show(txt, current_p)		
+				self.show(txt, current_p)		
 				self.last_distance = m  
 		else:
 			if m > 1:
@@ -68,3 +64,7 @@ class DistanceToolTip(ToolTip):
 				super(DistanceToolTip, self).show(txt, current_p)		  	
 				self.last_distance = m
 				self.showing = True
+
+	def deactivate(self):
+		self.deactivate()
+		self.showing = False
