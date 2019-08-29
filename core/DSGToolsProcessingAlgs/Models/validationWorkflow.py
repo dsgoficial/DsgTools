69821f7587c99c069ca37a196569c14fd758efee
esperidiao <joao.p2709@gmail.com>
2019-08-29 18:50:08 -0300

    model request and export methods added

--- core/DSGToolsProcessingAlgs/Models/validationWorkflow.py
+++ core/DSGToolsProcessingAlgs/Models/validationWorkflow.py
@@ -20,9 +20,17 @@
  ***************************************************************************/
 """
 
+import os, json
+
 from qgis.PyQt.QtCore import QObject
 
+from DsgTools.core.DSGToolsProcessingAlgs.Models.dsgToolsProcessingModel import DsgToolsProcessingModel
+
 class ValidationWorkflow(QObject):
+    """
+    Works as a multi-model runner. Understands all models' parameters as an
+    output vector layer.
+    """
     def __init__(self, parameters):
         """
         Class constructor. Materializes an workflow set of parameters.
@@ -36,22 +44,17 @@ class ValidationWorkflow(QObject):
             )
         self._param = parameters
 
-    def validateParameters(self, parameters=None):
+    def validateParameters(self, parameters):
         """
         Validates a set of parameters for a valid Workflow.
         :param parameters: (dict) map of workflow attributes to be validated.
         :return: (str) invalidation reason.
         """
-        parameters = parameters or self._param
         if "displayName" not in parameters or not parameters["displayName"]:
             # this is not a mandatory item, but it defaults to a value
             parameters["displayName"] = self.tr("DSGTools Validation Workflow")
         if "models" not in parameters or not parameters["models"]:
-            self.tr("This worflow seems to have no models associated to it.")
-        for modelParam in parameters["models"]:
-            msg = self.validateModelParameters(modelParam)
-            if msg:
-                return msg
+            return self.tr("This workflow seems to have no models associated to it.")
         # if "flagLayer" not in parameters or not parameters["flagLayer"]:
         #     self.tr("No flag layer was provided.")
         # if "historyLayer" not in parameters or not parameters["historyLayer"]:
@@ -69,9 +72,52 @@ class ValidationWorkflow(QObject):
     def models(self):
         """
         Model parameters defined to run in this workflow.
-        :return: (dict) models' parameters.
+        :return: (dict) models maps to valid and invalid models.
         """
-        return self._param["models"] if "models" in self._param else ""
+        models = {"valid" : dict(), "invalid" : dict()}
+        for modelName, modelParam in self._param["models"].items():
+            model = DsgToolsProcessingModel(modelParam)
+            if not model.isValid():
+                models["invalid"][modelName] = model.validateParameters(modelParam)
+            else:
+                models["valid"][modelName] = model
+        return models
+
+    def validModels(self):
+        """
+        Returns all valid models from workflow parameters.
+        :return: (dict) models maps to valid and invalid models.
+        """
+        models = dict()
+        for modelName, modelParam in self._param["models"].items():
+            model = DsgToolsProcessingModel(modelParam)
+            if model.isValid():
+                models[modelName] = model
+        return models
+
+    def invalidModels(self):
+        """
+        Returns all valid models from workflow parameters.
+        :return: (dict) models maps invalid models to their invalidation reason.
+        """
+        models = dict()
+        for modelName, modelParam in self._param["models"].items():
+            model = DsgToolsProcessingModel(modelParam)
+            if not model.isValid():
+                models[modelName] = model.validateParameters(modelParam)
+        return models
+
+    def hasInvalidModel(self):
+        """
+        Checks if any of the nested models is invalid.
+        :return: (bool) if there are invalid models.
+        """
+        models = dict()
+        for modelName, modelParam in self._param["models"].items():
+            model = DsgToolsProcessingModel(modelParam)
+            if not model.isValid():
+                return True
+        return False
 
     # def flagLayer(self):
     #     """
@@ -87,3 +133,20 @@ class ValidationWorkflow(QObject):
     #     """
     #     return self._param["flagLayer"] if "flagLayer" in self._param else ""
 
+    def export(self, filepath):
+        """
+        Dumps workflow's parameters as a JSON file.
+        :param filepath: (str) path to JSON file.
+        :return: (bool) operation success.
+        """
+        with open(filepath, "w", encoding="utf-8") as fp:
+            fp.write(json.dumps(self._param, sort_keys=True, indent=4))
+        return os.path.exists(filepath)
+
+    def exportAsDict(self):
+        """
+        Dumps model parameters as a JSON file.
+        :param filepath: (str) path to JSON file.
+        :return: (dict) DSGTools processing model definitions.
+        """
+        return self._param
