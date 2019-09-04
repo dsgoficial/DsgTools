54676466d622f2ddb12902ccc2090ff0a2caa0c9
esperidiao <joao.p2709@gmail.com>
2019-09-04 16:24:01 -0300

    added resize event, validation and table reading methods

--- gui/ProductionTools/Toolboxes/ValidationToolbox/workflowSetupDialog.py
+++ gui/ProductionTools/Toolboxes/ValidationToolbox/workflowSetupDialog.py
@@ -25,11 +25,16 @@ import os, json
 from datetime import datetime
 
 from qgis.PyQt import uic
+from qgis.core import Qgis
+from qgis.gui import QgsMessageBar
+from qgis.PyQt.QtCore import QSize, QCoreApplication
 from qgis.PyQt.QtWidgets import (QDialog,
                                  QComboBox,
                                  QCheckBox,
                                  QLineEdit,
+                                 QFileDialog,
                                  QTableWidgetItem)
+from processing.modeler.ModelerUtils import ModelerUtils
 
 from DsgTools.gui.CustomWidgets.SelectionWidgets.selectFileWidget import SelectFileWidget
 
@@ -38,6 +43,19 @@ FORM_CLASS, _ = uic.loadUiType(
 )
 
 class WorkflowSetupDialog(QDialog, FORM_CLASS):
+    __qgisModelPath__ = ModelerUtils.modelsFolders()[0]
+    ON_FLAGS_HALT, ON_FLAGS_WARN, ON_FLAGS_IGNORE = range(3)
+    onFlagsDisplayNameMap = {
+        ON_FLAGS_HALT : QCoreApplication.translate('WorkflowSetupDialog', "Halt"),
+        ON_FLAGS_WARN : QCoreApplication.translate('WorkflowSetupDialog', "Warn"),
+        ON_FLAGS_IGNORE : QCoreApplication.translate('WorkflowSetupDialog', "Ignore")
+    }
+    onFlagsValueMap = {
+        ON_FLAGS_HALT : "halt",
+        ON_FLAGS_WARN : "warn",
+        ON_FLAGS_IGNORE : "ignore"
+    }
+
     def __init__(self, parent=None):
         """
         Class constructor.
@@ -48,24 +66,39 @@ class WorkflowSetupDialog(QDialog, FORM_CLASS):
         super(WorkflowSetupDialog, self).__init__(parent)
         self.parent = parent
         self.setupUi(self)
+        self.messageBar = QgsMessageBar(self)
         self.orderedTableWidget.setHeaders({
-            "Model name" : {
+            self.tr("Model name") : {
                 "type" : "widget",
                 "class" : self.modelNameWidget
             },
-            "Model source" : {
+            self.tr("Model source") : {
                 "type" : "widget",
                 "class" : self.modelWidget
             },
-            "On flags" : {
+            self.tr("On flags") : {
                 "type" : "widget",
                 "class" : self.onFlagsWidget
             },
-            "Load output" : {
+            self.tr("Load output") : {
                 "type" : "widget",
                 "class" : self.loadOutputWidget
             }
         })
+
+    def resizeEvent(self, e):
+        """
+        Reimplementation in order to use this window's resize event.
+        On this object, this method makes sure that message bar is always the
+        same size as the window.
+        :param e: (QResizeEvent) resize event.
+        """
+        self.messageBar.resize(
+            QSize(
+                self.geometry().size().width(),
+                self.messageBar.geometry().height()
+            )
+        )
 
     def modelNameWidget(self):
         """
@@ -105,9 +138,9 @@ class WorkflowSetupDialog(QDialog, FORM_CLASS):
         """
         combo = QComboBox()
         combo.addItems([
-            self.tr("Halt"),
-            self.tr("Warn"),
-            self.tr("Ignore")
+            self.onFlagsDisplayNameMap[self.ON_FLAGS_HALT],
+            self.onFlagsDisplayNameMap[self.ON_FLAGS_WARN],
+            self.onFlagsDisplayNameMap[self.ON_FLAGS_IGNORE]
         ])
         return combo
 
@@ -159,20 +192,77 @@ class WorkflowSetupDialog(QDialog, FORM_CLASS):
         """
         return self.versionLineEdit.text()
 
-    def readModels(self):
+    def modelCount(self):
+        """
+        Reads the amount of models (rows added) the user intend to use.
+        :return: (int) model count.
+        """
+        return self.orderedTableWidget.rowCount()
+
+    def readRow(self, row):
+        """
+        Reads a row's contents and set it as a DsgToolsProcessingModel set of
+        parameters.
+        :return: (dict) parameters map.
+        """
+        contents = self.orderedTableWidget.row(row)
+        filepath = contents[self.tr("Model source")].lineEdit.text()
+        onFlagsIdx = contents[self.tr("On flags")].currentIndex()
+        if not os.path.exists(filepath):
+            raise Exception(
+                self.tr("Model from row {0} does not exist. Check filepath.")\
+                    .format(row + 1)
+            )
+        with open(filepath, "r", encoding="utf-8") as f:
+            xml = f.read()
+        return {
+            "displayName" : contents[self.tr("Model name")].text().strip(),
+            "flags" : {
+                "onFlagsRaised" : self.onFlagsValueMap[onFlagsIdx],
+                "loadOutput" : contents[self.tr("Load output" )].isChecked()
+            },
+            "source" : {
+                "type" : "xml",
+                "data" : xml
+            }
+        }
+
+    def validateRowContents(self, contents):
+        """
+        Checks if all attributes read from a row are valid.
+        :param contents: (dict) map to (already read) row contents.
+        :return: (str) invalidation reason
+        """
+        if contents["displayName"] == "":
+            return self.tr("Missing model's name.")
+        if contents["source"]["data"] == "":
+            return self.tr("Model is empty or file was not found.")
+        return ""
+
+    def models(self):
         """
         Reads all table contents and sets it as a DsgToolsProcessingAlgorithm's
         set of parameters.
         :return: (dict) map to each model's set of parameters.
         """
-        return dict()
+        models = dict()
+        for row in range(self.modelCount()):
+            contents = self.readRow(row)
+            models[contents["displayName"]] = contents
+        return models
 
-    def validate(self):
+    def validateModels(self):
         """
-        Checks if all filled data generates a valid Workflow object.
-        :return: (bool) validation status.
+        Check if each row on table has a valid input.
+        :return: (str) invalidation reason.
         """
-        return False
+        for row in range(self.modelCount()):
+            msg = self.validateRowContents(self.readRow(row))
+            if msg:
+                return "Row {row}: '{error}'".format(row=row, error=msg)
+        if len(self.models()) != self.modelCount():
+            return self.tr("Check if no model name is repeated.")
+        return ""
 
     def workflowParameterMap(self):
         """
@@ -180,7 +270,7 @@ class WorkflowSetupDialog(QDialog, FORM_CLASS):
         """
         return {
             "displayName" : self.workflowName(),
-            "models" : self.readModels(),
+            "models" : self.models(),
             "metadata" : {
                 "author" : self.author(),
                 "version" : self.version(),
@@ -188,8 +278,31 @@ class WorkflowSetupDialog(QDialog, FORM_CLASS):
             }
         }
 
+    def validate(self):
+        """
+        Checks if all filled data generates a valid Workflow object.
+        :return: (bool) validation status.
+        """
+        if self.workflowName() == "":
+            return self.tr("Workflow's name needs to be filled.")
+        if self.author() == "":
+            return self.tr("Workflow's author needs to be filled.")
+        if self.version() == "":
+            return self.tr("Workflow's version needs to be filled.")
+        msg = self.validateModels()
+        if msg != "":
+            return msg
+        return ""
+
     def export(self, filepath):
         """
         Exports current input data as a workflow JSON, IF input is valid.
+        :return: (bool) operation success.
         """
-        pass
+        msg = self.validate()
+        if msg != "":
+            self.messageBar.pushMessage(
+                self.tr('Invalid workflow'), msg, level=Qgis.Critical, duration=5
+            )
+            return False
+        
