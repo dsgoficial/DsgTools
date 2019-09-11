355b1f8bcc05fbd7245d9a10cce4b1d387e33749
phborba <philipeborba@gmail.com>
2019-09-11 14:31:11 -0300

    identify out of bounds angles in buildings

--- core/DSGToolsProcessingAlgs/dsgtoolsProcessingAlgorithmProvider.py
+++ core/DSGToolsProcessingAlgs/dsgtoolsProcessingAlgorithmProvider.py
@@ -147,6 +147,8 @@ from DsgTools.core.DSGToolsProcessingAlgs.Algs.LayerManagementAlgs.applyStylesFr
     ApplyStylesFromDatabaseToLayersAlgorithm
 
 from DsgTools.core.DSGToolsProcessingAlgs.Algs.OtherAlgs.singleOutputUnitTestAlgorithm import SingleOutputUnitTestAlgorithm
+from DsgTools.core.DSGToolsProcessingAlgs.Algs.ValidationAlgs.identifyWrongBuildingAnglesAlgorithm import \
+    IdentifyWrongBuildingAnglesAlgorithm
 
 from processing.core.ProcessingConfig import ProcessingConfig, Setting
 
@@ -214,7 +216,8 @@ class DSGToolsProcessingAlgorithmProvider(QgsProcessingProvider):
                     MatchAndApplyQmlStylesToLayersAlgorithm(),
                     ApplyStylesFromDatabaseToLayersAlgorithm(),
                     SingleOutputUnitTestAlgorithm(),
-                    ExportToMemoryLayer()
+                    ExportToMemoryLayer(),
+                    IdentifyWrongBuildingAnglesAlgorithm()
                 ]
         return algList
 
