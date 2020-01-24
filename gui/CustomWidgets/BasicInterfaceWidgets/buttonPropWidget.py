e112255b62c474cbe5552266f7187af3229ead59
esperidiao <joao.p2709@gmail.com>
2020-01-24 11:48:42 -0300

    undo and add button working on button setup gui

--- gui/CustomWidgets/BasicInterfaceWidgets/buttonPropWidget.py
+++ gui/CustomWidgets/BasicInterfaceWidgets/buttonPropWidget.py
@@ -20,106 +20,153 @@
  *                                                                         *
  ***************************************************************************/
 """
-from builtins import map
+
 import os
 
 from qgis.core import QgsMessageLog
 
-# Qt imports
 from qgis.PyQt import uic
-from qgis.PyQt.QtCore import pyqtSlot, pyqtSignal, QSettings, Qt
-from qgis.PyQt.QtSql import QSqlQuery
-from qgis.PyQt.QtWidgets import QDialog, QFileDialog, QMessageBox, QRadioButton
 from qgis.PyQt.QtGui import QColor
+from qgis.PyQt.QtSql import QSqlQuery
+from qgis.PyQt.QtCore import pyqtSlot, pyqtSignal, QSettings, Qt
+from qgis.PyQt.QtWidgets import QDialog, QFileDialog, QMessageBox, QRadioButton
 
+from DsgTools.gui.ProductionTools.Toolboxes.FieldToolBox.customButtonSetup import CustomButtonSetup, CustomFeatureButton
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'buttonPropWidget.ui'))
 
 class ButtonPropWidget(QDialog, FORM_CLASS):
-    def __init__(self, parent = None):
-        """Constructor."""
+    def __init__(self, parent=None, buttonSetup=None):
+        """
+        Class constructor.
+        :param parent: (QtWidgets.*) any widget that 'contains' this tool.
+        :param buttonSetup: (CustomButtonSetup) object that handles all
+                            buttons displayed and configured through this GUI.
+        """
         super(ButtonPropWidget, self).__init__(parent)
         self.setupUi(self)
-    #     self.customCategoryCheckBox.hide()
-    #     self.customCategoryLineEdit.hide()
-    
-    # @pyqtSlot(bool, name = 'on_colorCheckBox_toggled')
-    # @pyqtSlot(bool, name = 'on_tooltipCheckBox_toggled')
-    # @pyqtSlot(bool, name = 'on_customCategoryCheckBox_toggled')
-    # @pyqtSlot(bool, name = 'on_shortcutCheckBox_toggled')
-    # def setEnabled(self, state):
-    #     """
-    #     Enables or disables 
-    #     """
-    #     if self.sender().objectName() == 'colorCheckBox':
-    #         self.mColorButton.setEnabled(state)
-    #     if self.sender().objectName() == 'tooltipCheckBox':
-    #         self.toolTipLineEdit.setEnabled(state)
-    #     if self.sender().objectName() == 'customCategoryCheckBox':
-    #         self.customCategoryLineEdit.setEnabled(state)
-    #     if self.sender().objectName() == 'shortcutCheckBox':
-    #         self.shortcutWidget.setEnabled(state)
-    
-    # def getParameterDict(self):
-    #     """
-    #     Returns a dict in the format:
-    #     {
-    #         'buttonColor':--color of the button--, 
-    #         'buttonToolTip':--button toolTip--, 
-    #         'buttonGroupTag':--group tag of the button--,
-    #         'buttonShortcut':--shortcut--
-    #     }
-    #     """
-    #     parameterDict = dict()
-    #     if self.colorCheckBox.checkState() == Qt.Checked:
-    #         parameterDict['buttonColor'] = ','.join(map(str,self.mColorButton.color().getRgb())) #must map to string
-    #     if self.tooltipCheckBox.checkState() == Qt.Checked:
-    #         parameterDict['buttonToolTip'] = self.toolTipLineEdit.text()
-    #     if self.customCategoryCheckBox.checkState() == Qt.Checked:
-    #         parameterDict['buttonGroupTag'] = self.customCategoryLineEdit.text()
-    #     if self.shortcutCheckBox.checkState() == Qt.Checked:
-    #         parameterDict['buttonShortcut'] = self.shortcutWidget.getShortcut()
-    #     if self.openFormCheckBox.checkState() == Qt.Checked:
-    #         parameterDict['openForm'] = True
-    #     return parameterDict
-    
-    # def setInterface(self, parameterDict):
-    #     """
-    #     Sets the interface with a dict in the format:
-    #     {
-    #         'buttonColor':--color of the button--, 
-    #         'buttonToolTip'--button toolTip--, 
-    #         'buttonGroupTag':--group tag of the button--,
-    #         'buttonShortcut':--shortcut for the button--,
-    #         'openForm':--open feature form when digitizing--
-    #     }
-    #     """
-    #     if 'buttonColor' in list(parameterDict.keys()):
-    #         self.colorCheckBox.setCheckState(Qt.Checked)
-    #         R,G,B,A = list(map(int,parameterDict['buttonColor'].split(','))) #QColor only accepts int values
-    #         self.mColorButton.setColor(QColor(R,G,B,A))
-    #     else:
-    #         self.colorCheckBox.setCheckState(Qt.Unchecked) #if 'buttonColor' isn't on dict keys, set colorCheckBox as unchecked
-    #     if 'buttonToolTip' in list(parameterDict.keys()):
-    #         self.tooltipCheckBox.setCheckState(Qt.Checked)
-    #         self.toolTipLineEdit.setText(parameterDict['buttonToolTip'])
-    #     else:
-    #         self.tooltipCheckBox.setCheckState(Qt.Unchecked) #if 'buttonToolTip' isn't on dict keys, set tooltipCheckBox as unchecked
-    #         self.toolTipLineEdit.setText('')
-    #     if 'buttonGroupTag' in list(parameterDict.keys()):
-    #         self.customCategoryCheckBox.setCheckState(Qt.Checked)
-    #         self.customCategoryLineEdit.setText(parameterDict['buttonGroupTag'])
-    #     else:
-    #         self.customCategoryCheckBox.setCheckState(Qt.Unchecked) #if 'buttonGroupTag' isn't on dict keys, set customCategoryCheckBox as unchecked
-    #         self.customCategoryLineEdit.setText('')
-    #     if 'buttonShortcut' in list(parameterDict.keys()):
-    #         self.shortcutCheckBox.setCheckState(Qt.Checked)
-    #         self.shortcutWidget.setShortcut(parameterDict['buttonShortcut'])
-    #     else:
-    #         self.shortcutCheckBox.setCheckState(Qt.Unchecked) #if 'buttonShortcut' isn't on dict keys, set shortcutCheckBox as unchecked
-    #         self.shortcutWidget.clearAll()
-    #     if 'openForm' in list(parameterDict.keys()):
-    #         self.openFormCheckBox.setCheckState(Qt.Checked)
-    #     else:
-    #         self.openFormCheckBox.setCheckState(Qt.Unchecked) #if 'openForm' isn't on dict keys, set openFormCheckBox as unchecked
+        self.buttonSetup = buttonSetup or CustomButtonSetup()
+        self.refresh()
+
+    def clear(self):
+        """
+        Clears all data from interface.
+        """
+        pass
+
+    def refresh(self):
+        """
+        Clears interface data and resets it to its default.
+        """
+        self.clear()
+        self.buttonComboBox.addItem(self.tr("Select a button..."))
+        self.buttonComboBox.addItems([
+            b.name() for b in self.buttonSetup.buttons()
+        ])
+
+    def readButton(self):
+        """
+        Reads data from the interface and sets it to a button object.
+        :return: (CustomFeatureButton) button read from the interface.
+        """
+        b = CustomFeatureButton()
+        b.setName(self.nameLineEdit.text())
+        b.setUseColor(self.colorCheckBox.isChecked())
+        if self.colorCheckBox.isChecked():
+            b.setColor(self.mColorButton.color().getRgb())
+        if self.tooltipCheckBox.isChecked():
+            b.setToolTip(self.toolTipLineEdit.text())
+        if self.categoryCheckBox.isChecked():
+            b.setCategory(self.categoryLineEdit.text())
+        if self.shortcutCheckBox.isChecked():
+            b.setShortcut(self.shortcutWidget.getShortcut())
+        b.setOpenForm(self.openFormCheckBox.isChecked())
+        return b
+
+    def currentButtonName(self):
+        """
+        Retrieves currently selected button on button combo box.
+        :return: (CustomFeatureButton) button read from the setup object.
+        """
+        text = self.buttonComboBox.currentText()
+        return text if text != self.tr("Select a button...") else ""
+
+    def currentButton(self):
+        """
+        Retrieves currently selected button on button combo box.
+        :return: (CustomFeatureButton) button read from the setup object.
+        """
+        if not self.currentButtonName():
+            button = CustomFeatureButton()
+            button.setName("")
+            return button
+        return self.buttonSetup.button(self.currentButtonName())
+
+    @pyqtSlot(int, name="on_buttonComboBox_currentIndexChanged")
+    def setCurrentButton(self, button):
+        """
+        Changes current active button.
+        :button: (CustomFeatureButton) button to be set as active.
+        """
+        isButton = bool(self.currentButtonName())
+        self.savePushButton.setEnabled(isButton)
+        self.undoPushButton.setEnabled(isButton)
+        self.removePushButton.setEnabled(isButton)
+        if isinstance(button, int):
+            # then this came from the signal input
+            button = self.currentButton()
+        self.buttonComboBox.setCurrentText(button.name())
+        self.nameLineEdit.setText(button.name())
+        self.colorCheckBox.setChecked(button.useColor())
+        if button.useColor():
+            col = button.color()
+            col = QColor(col) if isinstance(col, str) else QColor(*col)
+            self.mColorButton.setColor(col)
+        self.tooltipCheckBox.setChecked(bool(button.toolTip()))
+        self.toolTipLineEdit.setText(button.toolTip())
+        self.categoryCheckBox.setChecked(bool(button.category()))
+        self.categoryLineEdit.setText(button.category())
+        self.shortcutCheckBox.setChecked(bool(button.shortcut()))
+        self.shortcutWidget.setShortcut(button.shortcut())
+        self.openFormCheckBox.setChecked(button.openForm())
+
+    @pyqtSlot(bool)
+    def on_savePushButton_clicked(self, props):
+        """
+        Current data will be store as current buttons properties
+        """
+        pass
+
+    @pyqtSlot(bool)
+    def on_undoPushButton_clicked(self):
+        """
+        Current data will be store as current buttons properties
+        """
+        self.setCurrentButton(self.currentButton())
+
+    @pyqtSlot(bool, name="on_addPushButton_clicked")
+    def addButton(self):
+        """
+        Adds a new button to the interface.
+        """
+        button = CustomFeatureButton()
+        name = button.name()
+        names = [b.name() for b in self.buttonSetup.buttons()]
+        if name in names:
+            i = 1
+            name = "{0} {1}".format(name, i)
+            while name in names:
+                name = "{0} {1}".format(button.name(), i)
+                i += 1
+            button.setName(name)
+        self.buttonSetup.addButton(button.properties())
+        self.buttonComboBox.addItem(name)
+        self.setCurrentButton(button)
+
+    @pyqtSlot(bool)
+    def on_removePushButton_clicked(self):
+        """
+        Current data will be store as current buttons properties
+        """
+        pass
