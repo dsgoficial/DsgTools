f8e2313d60c65795696497a20e4613e248080827
franciscocamellon <franciscocamellon@gmail.com>
2020-05-27 14:55:51 -0300

    remove a fixed param. pep8 fixes. algRunner method reference fixed. remove flag issues.

--- core/DSGToolsProcessingAlgs/Algs/ValidationAlgs/topologicalDouglasSimplificationAreasAlgorithm.py
+++ core/DSGToolsProcessingAlgs/Algs/ValidationAlgs/topologicalDouglasSimplificationAreasAlgorithm.py
@@ -47,7 +47,6 @@ class TopologicalDouglasSimplificationAreasAlgorithm(ValidationAlgorithm):
     """
     INPUTLAYERS = 'INPUTLAYERS'
     SELECTED = 'SELECTED'
-    METHOD = 'METHOD'
     TOLERANCE = 'TOLERANCE'
     FLAGS = 'FLAGS'
     OUTPUT = 'OUTPUT'
@@ -59,11 +58,11 @@ class TopologicalDouglasSimplificationAreasAlgorithm(ValidationAlgorithm):
         self.addParameter(
             QgsProcessingParameterMultipleLayers(
                 self.INPUTLAYERS,
-                self.tr('Linestring Layers'),
+                self.tr('Polygon Layers'),
                 QgsProcessing.TypeVectorPolygon
             )
         )
-        """
+
         self.addParameter(
             QgsProcessingParameterBoolean(
                 self.SELECTED,
@@ -71,17 +70,6 @@ class TopologicalDouglasSimplificationAreasAlgorithm(ValidationAlgorithm):
             )
         )
 
-        self.addParameter(
-            QgsProcessingParameterEnum(
-                self.METHOD,
-                self.tr('Simplification method'),
-                {'Distance (Douglas Peucker)': 0, 'Snap to grid': 1,
-                    'Area (Visvalingam)': 2},
-                allowMultiple=False,
-                defaultValue=0
-            )
-        )
-        """
         self.addParameter(
             QgsProcessingParameterNumber(
                 self.TOLERANCE,
@@ -119,11 +107,10 @@ class TopologicalDouglasSimplificationAreasAlgorithm(ValidationAlgorithm):
             raise QgsProcessingException(
                 self.invalidSourceError(parameters, self.INPUTLAYERS))
         onlySelected = self.parameterAsBool(parameters, self.SELECTED, context)
-        method = 0  # self.parameterAsEnum(parameters, self.METHOD, context)
+        method = 0  # fixed simplification method
         tolerance = self.parameterAsDouble(parameters, self.TOLERANCE, context)
         self.prepareFlagSink(
             parameters, inputLyrList[0], QgsWkbTypes.MultiPolygon, context)
-
 
         multiStepFeedback = QgsProcessingMultiStepFeedback(3, feedback)
         multiStepFeedback.setCurrentStep(0)
@@ -135,57 +122,20 @@ class TopologicalDouglasSimplificationAreasAlgorithm(ValidationAlgorithm):
         multiStepFeedback.pushInfo(
             self.tr('Running clean on unified layer...'))
 
-        simplifiedCoverage, error = algRunner.runDouglasLinesSimplification(
+        simplifiedCoverage = algRunner.runDouglasSimplification(
             coverage,
             method,
             tolerance,
             context,
             feedback=multiStepFeedback,
-            #onlySelected=onlySelected
+            onlySelected=onlySelected
         )
         multiStepFeedback.setCurrentStep(2)
         multiStepFeedback.pushInfo(self.tr('Updating original layer...'))
         layerHandler.updateOriginalLayersFromUnifiedLayer(
             inputLyrList, simplifiedCoverage, feedback=multiStepFeedback)
-        self.flagCoverageIssues(simplifiedCoverage, error, feedback)
 
-        return {self.INPUTLAYERS : inputLyrList, self.FLAGS : self.flag_id}
-
-    def flagCoverageIssues(self, cleanedCoverage, error, feedback):
-        """
-        flags overlaps and gaps in the coverage
-        """
-        overlapDict = dict()
-        for feat in cleanedCoverage.getFeatures():
-            if feedback.isCanceled():
-                break
-            geom = feat.geometry()
-            geomKey = geom.asWkb()
-            if geomKey not in overlapDict:
-                overlapDict[geomKey] = []
-            overlapDict[geomKey].append(feat)
-        for geomKey, featList in overlapDict.items():
-            if feedback.isCanceled():
-                break
-            if len(featList) > 1:
-                txtList = []
-                for i in featList:
-                    txtList += ['{0} (id={1})'.format(i['layer'], i['featid'])]
-                txt = ', '.join(txtList)
-                self.flagFeature(featList[0].geometry(), self.tr(
-                    'Features from {0} overlap').format(txt))
-            elif len(featList) == 1:
-                attrList = featList[0].attributes()
-                if attrList == len(attrList)*[None]:
-                    self.flagFeature(featList[0].geometry(), self.tr(
-                        'Gap in coverage.'))
-
-        if error:
-            for feat in error.getFeatures():
-                if feedback.isCanceled():
-                    break
-                self.flagFeature(feat.geometry(), self.tr(
-                    'Clean error on coverage.'))
+        return {self.INPUTLAYERS : inputLyrList}
 
     def name(self):
         """
@@ -222,8 +172,14 @@ class TopologicalDouglasSimplificationAreasAlgorithm(ValidationAlgorithm):
         return 'DSGTools: Quality Assurance Tools (Topological Processes)'
 
     def tr(self, string):
+        """
+        Returns a translatable string with the self.tr() function.
+        """
         return QCoreApplication.translate(
             'TopologicalDouglasSimplificationAreasAlgorithm', string)
 
     def createInstance(self):
+        """
+        Must return a new copy of your algorithm.
+        """
         return TopologicalDouglasSimplificationAreasAlgorithm()
