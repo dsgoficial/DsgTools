870db1117b583579091287189d39991dc612ab23
franciscocamellon <franciscocamellon@gmail.com>
2020-12-21 11:42:20 -0300

    Wrapper

---
+++ gui/ProcessingUI/identifyWrongSetOfAttributesWrapper.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ DsgTools
+                                 A QGIS plugin
+ Brazilian Army Cartographic Production Tools
+                              -------------------
+        begin                : 2020-07-27
+        git sha              : $Format:%H$
+        copyright            : (C) 2020 by  Francisco Alves Camello Neto -
+                                    Surveying Technician @ Brazilian Army
+        email                : camello.francisco@eb.mil.br
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
