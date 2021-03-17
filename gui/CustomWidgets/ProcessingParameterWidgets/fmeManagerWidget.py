c1020fbd7d58caa4ac6372ace9141f4810dff5ff
Eliton <elitonfilho@gmail.com>
2021-03-17 16:01:22 -0300

    fme workspace dispatcher working v1/v2

--- gui/CustomWidgets/ProcessingParameterWidgets/fmeManagerWidget.py
+++ gui/CustomWidgets/ProcessingParameterWidgets/fmeManagerWidget.py
@@ -20,19 +20,23 @@
  *                                                                         *
  ***************************************************************************/
 """
-import os, json, requests
+import os
+import json
+import requests
 
 # Qt imports
 from qgis.PyQt import QtWidgets, uic
-from qgis.PyQt.QtCore import pyqtSlot, Qt, QSettings
+from qgis.PyQt.QtCore import pyqtSlot, Qt, QSettings, QSize
+from qgis.gui import QgsMessageBar
 
 from DsgTools.core.Utils.utils import Utils
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'fmeManagerWidget.ui'))
 
+
 class FMEManagerWidget(QtWidgets.QWidget, FORM_CLASS):
-    def __init__(self, parent = None):
+    def __init__(self, parent=None):
         """Constructor."""
         super(FMEManagerWidget, self).__init__(parent=parent)
         self.setupUi(self)
@@ -40,45 +44,94 @@ class FMEManagerWidget(QtWidgets.QWidget, FORM_CLASS):
         self.interfaceDict = {}
         self.server = ''
         self.proxy_dict, self.auth = Utils().get_proxy_config()
+        self.versionComboBox.addItems(['v1', 'v2'])
+        self.messabe_bar = QgsMessageBar(self)
 
     def getCurrentWorkspace(self):
         idx = self.workspaceComboBox.currentIndex()
         return self.workspaceList[idx]
-    
+
     @pyqtSlot(int)
     def on_workspaceComboBox_currentIndexChanged(self):
+        """
+        Fetch necessary parameters from selected workspace and
+        insert a QLineEdit for each parameter
+        """
         self.clearLayout()
         workspace = self.getCurrentWorkspace()
-        for parameter in [x for x in workspace['parametros'] if x != 'LOG_FILE']:
+        try:
+            parameters = workspace['parameters'] if self.version == 'v1' else workspace['parametros']
+        except KeyError:
+            parameters = []
+            self.clearLayout()
+        for parameter in parameters:
             newLabel = QtWidgets.QLabel(parameter)
             self.verticalLayout_2.addWidget(newLabel)
             newLineEdit = QtWidgets.QLineEdit()
             self.interfaceDict[parameter] = newLineEdit
             self.verticalLayout_2.addWidget(newLineEdit)
-    
+
     def clearLayout(self):
         for i in reversed(range(self.verticalLayout_2.count())):
             self.verticalLayout_2.itemAt(i).widget().setParent(None)
-    
+
+    def resizeEvent(self, e):
+        """
+        Resize QgsMessageBar to widget's width
+        """
+        self.messabe_bar.resize(
+            QSize(
+                self.geometry().size().width(),
+                30
+            )
+        )
+
     @pyqtSlot(bool)
     def on_loadPushButton_clicked(self):
+        """
+        Sync available workspaces from server and display
+        these workspaces on workspaceComboBox
+        """
         self.server = self.serverLineEdit.text()
+        self.version = self.versionComboBox.currentText()
         self.workspaceComboBox.clear()
-        url = '{server}/api/rotinas'.format(server=self.server)
-        try:
-            self.workspaceList = requests.get(
-                url,
-                proxies=self.proxy_dict,
-                auth=self.auth,
-                timeout=8
+        self.clearLayout()
+        self.workspaceList = []
+        if self.version == 'v1':
+            url = '{server}/versions?last=true'.format(server=self.server)
+            try:
+                self.workspaceList = requests.get(
+                    url,
+                    proxies=self.proxy_dict,
+                    auth=self.auth,
+                    timeout=8
+                ).json()['data']
+            except:
+                self.messabe_bar.pushMessage(self.tr('Workspaces not found'))
+            for workspace in self.workspaceList:
+                self.workspaceComboBox.addItem(
+                    "{name} ({description})".format(
+                        name=workspace['workspace_name'],
+                        description=workspace['workspace_description']
+                    )
+                )
+        elif self.version == 'v2':
+            url = '{server}/api/rotinas'.format(server=self.server)
+            try:
+                self.workspaceList = requests.get(
+                    url,
+                    proxies=self.proxy_dict,
+                    auth=self.auth,
+                    timeout=8,
+                    verify=False
                 ).json()['dados']
-        except:
-            pass
-        for workspace in self.workspaceList:
-            self.workspaceComboBox.addItem(
-                "{name} ({description})".format(
-                    name=workspace['rotina'],
-                    description=workspace['descricao']
+            except:
+                self.messabe_bar.pushMessage(self.tr('Workspaces not found'))
+            for workspace in self.workspaceList:
+                self.workspaceComboBox.addItem(
+                    "{name} ({description})".format(
+                        name=workspace['rotina'],
+                        description=workspace['descricao']
                     )
                 )
 
@@ -89,16 +142,27 @@ class FMEManagerWidget(QtWidgets.QWidget, FORM_CLASS):
         if self.server == '':
             return False
         return True
-    
+
     def getParameters(self):
+        """
+        Returns necessary parameters for running the algorithm
+        """
         workspace = self.getCurrentWorkspace()
         workspace_id = workspace['id']
-        parameters = {'parametros':{key:value.text() for key, value in self.interfaceDict.items()}}
+        if self.version == 'v1':
+            parameters = {'parameters': {
+                key: value.text() for key, value in self.interfaceDict.items()
+                }}
+        elif self.version == 'v2':
+            parameters = {'parametros': {
+                key: value.text() for key, value in self.interfaceDict.items()
+                }}   
         returnDict = {
-            'server':self.server,
-            'workspace_id':workspace_id,
-            'parameters':parameters,
-            'auth' : self.auth,
-            'proxy_dict' : self.proxy_dict
+            'version': self.version,
+            'server': self.server,
+            'workspace_id': workspace_id,
+            'parameters': parameters,
+            'auth': self.auth,
+            'proxy_dict': self.proxy_dict
         }
         return returnDict
