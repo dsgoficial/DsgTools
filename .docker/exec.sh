879f001fa499a1d7e849849a5a5dc05e17d28237
phborba <philipeborba@gmail.com>
2021-08-18 15:51:31 -0300

    first commit on ci release

---
+++ .docker/exec.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker exec -t dsgtools-testing-env sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_image_processing"
+docker exec -it dsgtools-testing-env sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_ValidationAlgorithms"
+docker exec -it dsgtools-testing-env sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_EnvironmentSetterAlgorithms"
+docker exec -it dsgtools-testing-env sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_CustomButtonSetup"
+docker exec -it dsgtools-testing-env sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_DsgToolsProcessingModel"
+docker exec -it dsgtools-testing-env sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_OtherAlgorithms"
