a5286b6e19498ca1c32c93916b2cc20116c8bca0
Philipe Borba <philipeborba@gmail.com>
2021-08-23 19:31:10 -0300

    changing the way docker is run

--- .docker/exec.sh
+++ .docker/exec.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker exec -t qgis sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_ValidationAlgorithms \
+docker run --rm dsgtools-testing-env sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_ValidationAlgorithms \
                                             && qgis_testrunner.sh tests.test_EnvironmentSetterAlgorithms \
                                             && qgis_testrunner.sh tests.test_CustomButtonSetup && \ 
                                             qgis_testrunner.sh tests.test_DsgToolsProcessingModel && \
