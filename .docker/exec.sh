a5df79b29c20642c1f119b16959c052c8c6dab30
phborba <philipeborba@gmail.com>
2021-08-23 20:21:53 -0300

    minor fix

--- .docker/exec.sh
+++ .docker/exec.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker-compose exec -T qgis sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_ValidationAlgorithms \
+echo "Running tests" && docker-compose exec -T qgis sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_ValidationAlgorithms \
                                             && qgis_testrunner.sh tests.test_EnvironmentSetterAlgorithms \
                                             && qgis_testrunner.sh tests.test_CustomButtonSetup && \ 
                                             qgis_testrunner.sh tests.test_DsgToolsProcessingModel && \
