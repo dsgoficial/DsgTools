78e5b4946d3f330789b22b136a0978aca7b22187
Philipe Borba <philipeborba@gmail.com>
2021-08-23 18:49:41 -0300

    script fix

--- .docker/exec.sh
+++ .docker/exec.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker exec -it dsgtools-testing-env sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_ValidationAlgorithms"
-docker exec -it dsgtools-testing-env sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_EnvironmentSetterAlgorithms"
-docker exec -it dsgtools-testing-env sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_CustomButtonSetup"
-docker exec -it dsgtools-testing-env sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_DsgToolsProcessingModel"
-docker exec -it dsgtools-testing-env sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_OtherAlgorithms"
+docker exec -it dsgtools-testing-env sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_ValidationAlgorithms \
+                                            && qgis_testrunner.sh tests.test_EnvironmentSetterAlgorithms \
+                                            && qgis_testrunner.sh tests.test_CustomButtonSetup && \ 
+                                            qgis_testrunner.sh tests.test_DsgToolsProcessingModel && \
+                                            qgis_testrunner.sh tests.test_OtherAlgorithms"
