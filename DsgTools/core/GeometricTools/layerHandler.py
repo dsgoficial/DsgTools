8e6141146468aaa6741f57c6ebdb8768bdc2fb9f
phborba <philipeborba@gmail.com>
2023-04-19 12:19:14 -0300

    ui improvement in update original features

--- DsgTools/core/GeometricTools/layerHandler.py
+++ DsgTools/core/GeometricTools/layerHandler.py
@@ -476,6 +476,9 @@ class LayerHandler(QObject):
         idsToRemove, featuresToAdd = set(), set()
         lyr.startEditing()
         lyr.beginEditCommand("Updating layer {0}".format(lyr.name()))
+        nSteps = len(inputDict)
+        if nSteps == 0 or feedback.isCanceled():
+            return
         localTotal = 100 / len(inputDict) if inputDict else 0
         futures = set()
         pool = concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count() - 1)
@@ -525,6 +528,8 @@ class LayerHandler(QObject):
             list(map(changeGeometryLambda, geometriesToChange))
             featuresToAdd = featuresToAdd.union(addedFeatures)
             idsToRemove = idsToRemove.union(deletedIds)
+            if current % 1000 == 0:
+                multiStepFeedback.pushInfo(self.tr(f"Evaluated {current}/{nSteps} results."))
             if feedback is not None:
                 feedback.setProgress(localTotal * current)
         lyr.addFeatures(list(featuresToAdd))
