c7b3de59af3be959db3f828f630bf4bc71e51a6e
phborba <philipeborba@gmail.com>
2025-03-12 09:46:43 -0300

    remove spaces

--- DsgTools/core/DSGToolsProcessingAlgs/Algs/OtherAlgs/batchRunAlgorithm.py
+++ DsgTools/core/DSGToolsProcessingAlgs/Algs/OtherAlgs/batchRunAlgorithm.py
@@ -109,7 +109,7 @@ class BatchRunAlgorithm(QgsProcessingAlgorithm):
         outputKey = self.parameterAsString(
             parameters, self.OUTPUT_LAYER_PARAMETER_NAME, context
         )
-        layerNameList = layerCsv.split(",") if layerCsv != "" else []
+        layerNameList = [i.strip() for i in layerCsv.split(",")] if layerCsv != "" else []
         nSteps = len(layerNameList)
         if not nSteps:
             _, flag_id = self.parameterAsSink(
